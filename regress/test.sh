fb2c08ebe105726fa8aae45c22adab8af2bdf8d4
fukachan <fukachan>
2001-01-19 15:23:00 +0000

    fix path

--- regress/test.sh
+++ regress/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo rm -f /usr/local/libexec/fml/fmlwrapper ; 
-sudo sh INSTALL.sh ; 
+sudo sh ../INSTALL.sh ; 
 cat example |\
 perl -w /usr/local/libexec/fml/fml.pl /var/spool/ml/elena
 
