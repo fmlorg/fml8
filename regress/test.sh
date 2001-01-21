0209ce97b6555088d8ed705980ef68e1b0b3fc9c
fukachan <fukachan>
2001-01-21 11:52:42 +0000

    more debug

--- regress/test.sh
+++ regress/test.sh
@@ -3,11 +3,14 @@
 # $Id$
 #
 
-cat example |\
-perl -w /usr/local/libexec/fml/fmlwrapper \
+cd .. || exit 1
+pwd
+cat regress/example |\
+perl -w fml/libexec/fmlwrapper \
 	--params pwd=$PWD \
-	-c $PWD/main.cf \
+	-c $PWD/regress/main.cf \
 	/var/spool/ml/elena
+echo $?
 
 exit 0
 
