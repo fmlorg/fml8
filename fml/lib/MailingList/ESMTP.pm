bd65f0d0d161adea47713832a4acc59b0c811034
fukachan <fukachan>
2001-01-27 11:38:59 +0000

    alias for SMTP class for convenience.

---
+++ fml/lib/MailingList/ESMTP.pm
@@ -0,0 +1,57 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $Id$
+# $FML$
+#
+
+package MailingList::ESMTP;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+use MailingList::SMTP;
+
+require Exporter;
+@ISA = qw(MailingList::SMTP Exporter);
+
+sub new
+{
+    my ($self) = @_;
+    $self->SUPER::new(@_);
+}
+
+
+=head1 NAME
+
+MailingList::ESMTP.pm - Extended SMTP class
+
+=head1 SYNOPSIS
+
+   use MailingList::ESMTP;
+   $service = new MailingList::ESMTP;
+   $service->deliver( ... );
+
+See L<MailingList::SMTP> for more details since this ESMTP class
+is an alias of SMTP class for convenience.
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+MailingList::ESMTP.pm appeared in fml5.
+
+=cut
+
+1;
