1e10bc3a1248f6ddb7055611e5c07909fd5621d4
fukachan <fukachan>
2001-01-31 23:16:12 +0000

    add program emulation function not by MD5.pm

--- fml/lib/FML/Checksum.pm
+++ fml/lib/FML/Checksum.pm
@@ -18,22 +18,63 @@ require Exporter;
 
 sub new
 {
-    my ($self) = @_;
+    my ($self, $args) = @_;
     my ($type) = ref($self) || $self;
     my $me     = {};
+
+    _init($me, $args);
+
     return bless $me, $type;
+}
+
+
+sub _init
+{
+    my ($self, $args) = @_;
+
+    if (defined $args->{ program }) {
+	$self->{ _program } = $args->{ program };
+    }
+
+    my $pkg = 'MD5';
+    eval qq{ require $pkg; $pkg->import();};
+
+    unless ($@) {
+	$self->{ _type } = 'native';
+    }
+    else {
+	use FML::Utils qw(search_program);
+	my $prog = search_program('md5') || search_program('md5sum');
+	if (defined $prog) {
+	    $self->{ _program } = $prog;
+	} 
+    }
 }
 
 
 sub md5
 {
     my ($self, $r_data) = @_;
+
+    if ($self->{ _type } eq 'native') {
+	$self->_md5_native($r_data);
+    }
+    else {
+	$self->_md5_by_program($r_data);
+    }
+
+}
+
+
+sub _md5_native
+{
+    my ($self, $r_data) = @_;
     my ($buf, $p, $pe);
 
     $pe = length($$r_data);
 
-    use MD5;
-    my $md5 = new MD5;
+    my $md5;
+    eval q{ $md5 = new MD5;};
     $md5->reset();
 
     $p = 0;
@@ -46,6 +87,34 @@ sub md5
 
     $md5->hexdigest();
 }
+
+
+sub _md5_by_program
+{
+    my ($self, $r_data) = @_;
+
+    if (defined $self->{ _program }) {
+	my $program = $self->{ _program };
+	
+	use FileHandle;
+	my ($rh, $wh) = FileHandle::pipe;
+
+	use IPC::Open2;
+	my $pid = open2($rh, $wh, $self->{ _program });
+	if (defined $pid) {
+	    print $wh $$r_data;
+	    close($wh);
+	    my $cksum = 0;
+	    sysread($rh, $cksum, 1024);
+	    $cksum =~ s/[\s\n]*$//;
+	    close($rh);
+	    return $cksum if $cksum;
+	}
+    }
+
+    undef;
+}
+
 
 
 =head1 NAME
