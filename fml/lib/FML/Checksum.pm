c196fd1a741b3e379da6f43940291e716d92a347
fukachan <fukachan>
2001-01-31 16:27:05 +0000

    md5 checksum

---
+++ fml/lib/FML/Checksum.pm
@@ -0,0 +1,84 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $Id$
+# $FML$
+#
+
+package FML::Checksum;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+require Exporter;
+@ISA = qw(Exporter);
+
+sub new
+{
+    my ($self) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = {};
+    return bless $me, $type;
+}
+
+
+sub md5
+{
+    my ($self, $r_data) = @_;
+    my ($buf, $p, $pe);
+
+    $pe = length($$r_data);
+
+    use MD5;
+    my $md5 = new MD5;
+    $md5->reset();
+
+    $p = 0;
+    while (1) {
+	last if $p > $pe;
+	$_  = substr($$r_data, $p, 128);
+	$p += 128;
+	$md5->add($_);
+    }
+
+    $md5->hexdigest();
+}
+
+
+=head1 NAME
+
+FML::__HERE_IS_YOUR_MODULE_NAME__.pm - what is this
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 CLASSES
+
+=head1 METHODS
+
+=item C<new()>
+
+... what is this ...
+
+=head1 AUTHOR
+
+__YOUR_NAME__
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 __YOUR_NAME__
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+FML::__HERE_IS_YOUR_MODULE_NAME__.pm appeared in fml5.
+
+=cut
+
+1;
