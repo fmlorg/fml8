941a7e8d0ad4b9512dae547c32d97999230053d2
fukachan <fukachan>
2001-02-06 03:02:12 +0000

    use cvs tree directly

--- regress/simulation/ticket/list_up.sh
+++ regress/simulation/ticket/list_up.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-exec /usr/local/libexec/fml/fmlticket /var/spool/ml/elena
+#
+# $Id$
+#
+
+pwd=`pwd`
+
+cd ../.. || exit 1;
+
+exec /usr/local/libexec/fml/fmlticket \
+		--params pwd=$PWD \
+		-c $pwd/main.cf \
+		-R open \
+		list /var/spool/ml/elena
+
 
