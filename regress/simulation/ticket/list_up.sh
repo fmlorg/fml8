36a2bc2806c2afd793cecfecf2f2830ca427a61f
fukachan <fukachan>
2001-02-10 07:46:45 +0000

    merge primitive ticket formatter

--- regress/simulation/ticket/list_up.sh
+++ regress/simulation/ticket/list_up.sh
@@ -3,17 +3,17 @@
 # $Id$
 #
 
+tmp=/tmp/$$
 pwd=`pwd`
+
+trap "rm -f $tmp" 0 1 3 15
 
 cd ../.. || exit 1;
 
-exec /usr/local/libexec/fml/fmlticket \
+/usr/local/libexec/fml/fmlticket \
 		--params pwd=$PWD \
 		-c $pwd/main.cf \
 		-R open \
-		list /var/spool/ml/elena |\
-		perl regress/simulation/ticket/format -D /var/spool/ml/elena
+		list /var/spool/ml/elena
 
-
-
-
+exit 0;
