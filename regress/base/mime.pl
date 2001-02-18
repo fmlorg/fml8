68f0651435b6ab5486ce884fdaaf10364194552c
fukachan <fukachan>
2001-02-18 11:41:17 +0000

    check quoted-printables check

--- regress/base/mime.pl
+++ regress/base/mime.pl
@@ -1,12 +1,17 @@
 #!/usr/local/bin/perl
+#
+# $Id$
+#
+
 
 use strict;
 use Carp;
 use FML::MIME qw(decode_mime_string encode_mime_string);
 use MIME::Base64;
+use MIME::QuotedPrint;
 
 
-&try_mime("");
+&try_mime(" ");
 &try_mime("　");
 &try_mime("\n");
 
@@ -16,15 +21,28 @@ exit 0;
 sub try_mime
 {
     my ($separator) = @_;
+    my $buf = "\n";
 
     my $orig_str = "うじゃ". $separator ."あじゃじゃ";
-    my $in_str   = encode_mime_string($orig_str);
-    my $out_str  = decode_mime_string($in_str, {charset => 'euc-japan'}), 
+    my $in_str_b64 = encode_mime_string($orig_str);
+    my $in_str_qp  = encode_mime_string($orig_str, { encode => 'qp' });
 
-    print "   >", $orig_str, "<\n";
-    print "   >", $in_str, "<\n";
-    print "   <", $out_str, "<\n";
-    print ($orig_str eq $out_str ? "ok\n" : "fail\n");
+    $buf .=  ">". $orig_str. "<\n";
+    $buf .=  ">". $in_str_b64. "<\n";
+    $buf .=  ">". $in_str_qp. "<\n";
+
+    my $out_str_b64 = 
+	decode_mime_string($in_str_b64, {charset => 'euc-japan'});
+    $buf .=  "<". $out_str_b64. "<\n";
+
+    my $out_str_qp = decode_mime_string($in_str_qp, {charset => 'euc-japan'});
+    $buf .=  "<". $out_str_qp. "<\n";
+
+    $buf =~ s/\n/\n   |/g;
+    print $buf, "\n";
+    print ($orig_str eq $out_str_b64 ? "base64 ... ok\n" : "fail\n");
+    print ($orig_str eq $out_str_qp  ? "qp     ... ok\n" : "fail\n");
 }
+
 
 1;
