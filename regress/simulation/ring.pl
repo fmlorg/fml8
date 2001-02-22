462078a1cd898037e54e989992a2a5857a8588e9
fukachan <fukachan>
2001-02-22 12:16:49 +0000

    test of file_name

--- regress/simulation/ring.pl
+++ regress/simulation/ring.pl
@@ -4,9 +4,25 @@ my $obj = new File::RingBuffer {
     directory => "/tmp/b"
 };
 
-my $fh = $obj->open;
-$_ = `date`;
-print $fh $_;
-close($fh);
 
-$obj->close;
+&uja($obj);
+
+my $obj = new File::RingBuffer {
+    directory => "/tmp/b",
+    file_name => "_smtplog.",
+};
+
+&uja($obj);
+
+
+sub uja
+{
+	my ($obj) = @_;
+
+	my $fh = $obj->open;
+	$_ = `date`;
+	print $fh $_;
+	close($fh);
+
+	$obj->close;
+}
