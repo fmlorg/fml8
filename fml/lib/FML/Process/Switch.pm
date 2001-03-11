7ba5f4d84e7c35f6ce2f44858b9d4b6bc6b04370
fukachan <fukachan>
2001-03-11 14:15:31 +0000

    clean up the argument parser to handle makefml in the special way

--- fml/lib/FML/Process/Switch.pm
+++ fml/lib/FML/Process/Switch.pm
@@ -88,28 +88,32 @@ sub main::Bootstrap2
     use File::Basename;
     my $myname            = basename($0); # inspect my name from $0
 
-    # 1.0 parse command line options (preliminary)
+    # 1.1 parse command line options (preliminary)
     use Getopt::Long;
     GetOptions(\%options, _module_specific_options($myname));
 
     # 2.1 analyze main.cf and get the result in $main_cf
     use Standalone;
-    my $main_cf = 
-      Standalone::load_cf($options{'c'} || $main_cf_file, $options{'params'});
+    $main_cf_file = $options{'c'} || $main_cf_file;
+    my $main_cf   = Standalone::load_cf($main_cf_file, $options{'params'});
 
-    # 2.2 parse @ARGV ..
-    my $cf = _parse_argv($main_cf);
+    # 2.2 parse @ARGV and get a list of configuration files
+    #     XXX $main_cf{ ml_home_dir } (e.g. /var/spool/ml/elena) is defined
+    #         if possible.
+    #     a) resolve $ml_name (e.g. expand "elena" to "/var/spool/ml/elena")
+    #     b) but we need special treatment in some cases e.g. makefml
+    my $cf = _parse_argv($myname, $main_cf);
 
     # 2.3 prepare @$cf
-    # XXX hmm, .. '/etc/fml/site_default_config.cf' is good ???
+    #     XXX hmm, .. '/etc/fml/site_default_config.cf' is good ???
     unshift(@$cf, '/etc/fml/site_default_config.cf');
     unshift(@$cf, $main_cf->{ default_config });
 
-    # 3. reset @INC 
+    # 3.1 set up @INC 
     unshift(@INC, split(/\s+/, $main_cf->{ lib_dir }));	    
     unshift(@INC, split(/\s+/, $main_cf->{ local_lib_dir }));
 
-    # 3.1 useful for e.g. "fmldoc"
+    # 3.2 useful for e.g. "fmldoc"
     $ENV{'PERL5LIB'} = $main_cf->{ lib_dir };
 
     # 4. debug
@@ -132,9 +136,8 @@ sub main::Bootstrap2
 	cf_list        => $cf,
 	options        => \%options,
 
-	# pass the original information to each process
-	argv           => \@argv,
-	ARGV           => \@ARGV,
+	argv           => \@argv, # pass the original @ARGV
+	ARGV           => \@ARGV, # @ARGV after getopts()
 
 	# options
 	need_ml_name   => _ml_name_is_required($myname),
@@ -148,13 +151,26 @@ sub main::Bootstrap2
 }
 
 
-# Descriptions: 
-#    Arguments: $self $args
-# Side Effects: 
-# Return Value: none
+# Descriptions: analyze argument vector
+#    Arguments: $main_cf_file_name
+# Side Effects: none
+# Return Value: ARRAY REFERENCE (a list of config.cf's)
 sub _parse_argv
 {
-    my ($main_cf) = @_;
+    my ($myname, $main_cf) = @_;
+
+    if ($myname eq 'makefml') {
+	_makefml_parse_argv($myname, $main_cf);
+    }
+    else {
+	_usual_parse_argv($myname, $main_cf);
+    }
+}
+
+
+sub _usual_parse_argv
+{
+    my ($myname, $main_cf) = @_;
     my $ml_home_prefix = $main_cf->{ ml_home_prefix };
     my $ml_home_dir    = '';
     my $found_cf       = 0;
@@ -162,6 +178,8 @@ sub _parse_argv
 
     # "elena" is translated to "/var/spool/ml/elena"
     for (@ARGV) {
+	# 1. for the first time
+	#   a) speculate "/var/spool/ml/$_" looks a $ml_home_dir ?
 	unless ($found_cf) {
 	    my $x = "$ml_home_prefix/$_";
 	    if (-d $x && -f "$x/config.cf") {
@@ -171,6 +189,7 @@ sub _parse_argv
 	    }
 	}
 
+	# 2. /var/spool/ml/elena looks a $ml_home_dir ?
 	if (-d $_) {
 	    $ml_home_dir = $_;
 	    if (-f "$_/config.cf") {
@@ -178,6 +197,7 @@ sub _parse_argv
 		$found_cf = 1;
 	    }
 	}
+	# 3. looks a file, so /var/spool/ml/elena/config.cf ?
 	elsif (-f $_) {
 	    push(@cf, $_);
 	}
@@ -185,6 +205,33 @@ sub _parse_argv
 
     # save $ml_home_dir value in $main_cf directly
     $main_cf->{ ml_home_dir } = $ml_home_dir;
+
+    \@cf;
+}
+
+
+sub _makefml_parse_argv
+{
+    my ($myname, $main_cf) = @_;
+    my $ml_home_prefix = $main_cf->{ ml_home_prefix };
+
+    # makefml specific syntax.
+    my ($command, $ml_name, @options) = @ARGV;
+    if ($command =~ /\-\>/) {
+	($command, @options) = @ARGV;
+	($ml_name, $command) = split('->', $command);
+    }
+    elsif ($command =~ /::/) {
+	($command, @options) = @ARGV;
+	($ml_name, $command) = split('::', $command);
+    }
+
+    # save $ml_home_dir value in $main_cf directly
+    $main_cf->{ ml_home_dir } = "$ml_home_prefix/$ml_name";
+
+    # config.cf
+    my $cf = "$ml_home_prefix/$ml_name/config.cf";
+    my @cf = ($cf);
 
     \@cf;
 }
@@ -259,13 +306,13 @@ sub _module_specific_options
 	return qw(debug! params=s -c=s -A=s -R=s);
     }
     elsif ($myname eq 'fmlconf') {
-	return qw(ctladdr! debug! params=s -c=s n!);
+	return qw(debug! params=s -c=s n!);
     }
     elsif ($myname eq 'fmldoc') {
-	return qw(ctladdr! debug! params=s -c=s n!);	
+	return qw(debug! params=s -c=s);	
     }
     elsif ($myname eq 'makefml') {
-	return qw(ctladdr! debug! params=s -c=s n!);	
+	return qw(debug! params=s -c=s);	
     }
     else {
 	croak "options for $myname are not defined.\n";
