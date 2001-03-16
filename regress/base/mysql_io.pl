038969e30b0ab1c26657d24734a71c534d8167f1
fukachan <fukachan>
2001-03-16 04:30:40 +0000

    use /usr/pkg/bin/perl by default, add args for new()

--- regress/base/mysql_io.pl
+++ regress/base/mysql_io.pl
@@ -1,6 +1,12 @@
+#!/usr/pkg/bin/perl
+#
+# $Id$
+#
+
+use strict;
 use IO::Adapter::MySQL::toymodel;
 
-my $obj = new IO::Adapter::MySQL::toymodel;
+my $obj = new IO::Adapter::MySQL::toymodel { ml_name => 'elena' };
 
 $obj->open({
     sql_user          => 'fukachan',
