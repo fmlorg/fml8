1fe6a37839d9679fc2b2914248bbac14cd89027c
fukachan <fukachan>
2001-03-18 01:53:17 +0000

    Initial revision

---
+++ fml/lib/SQL/Schema/toymodel.pm
@@ -0,0 +1,29 @@
+#-*- perl -*-
+#
+# Copyright (C) 2000 Ken'ichi Fukamachi
+#          All rights reserved. 
+#
+# $Id$
+#
+
+
+package IO::Adapter::MySQL::toymodel;
+
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+=head1 NAME
+
+IO::Adapter::MySQL::toymodel - toymodel by SQL
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=cut
+
+
+1;
