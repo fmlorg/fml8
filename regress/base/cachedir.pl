55627edc40771ef5988ef21d35a2ff64d148e2f2
fukachan <fukachan>
2001-04-04 12:27:30 +0000

    sync w/ modified File::Cache

--- regress/base/cachedir.pl
+++ regress/base/cachedir.pl
@@ -5,7 +5,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML$
+# $FML: cachedir.pl,v 1.1 2001/04/04 03:10:09 fukachan Exp $
 #
 
 -d "/tmp/b" || mkdir("/tmp/b", 0755);
@@ -15,7 +15,7 @@ use File::CacheDir;
 
 my $obj = new File::CacheDir {
     directory => "/tmp/b"
-};
+    };
 
 
 &simple_write_to_ring_buffer($obj);
@@ -29,8 +29,8 @@ my $obj = new File::CacheDir {
 
 
 my $obj = new File::CacheDir {
-    directory => "/tmp/b",
-    limited_by => 'time',
+    directory  => "/tmp/b",
+    cache_type => 'temporal',
 };
 
 &simple_write_to_ring_buffer($obj);
@@ -38,12 +38,18 @@ my $obj = new File::CacheDir {
 
 sub simple_write_to_ring_buffer
 {
-	my ($obj) = @_;
+    my ($obj) = @_;
 
+    if (defined $obj) {
 	my $fh = $obj->open;
 	$_ = `date`;
 	print $fh $_;
 	close($fh);
-
 	$obj->close;
+	system "ls -lR /tmp/b";
+    }
+    else {
+	print "Error: undefined object: ";
+	print $@, "\n";
+    }
 }
