d01124c714b2c483d2df1757742c2ecdc18c05d8
fukachan <fukachan>
2001-04-05 10:39:08 +0000

    emulate unsubscribe for "signoff" command

---
+++ fml/lib/FML/Command/signoff.pm
@@ -0,0 +1,53 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $Id$
+# $FML: signoff.pm,v 1.5 2001/04/03 09:45:42 fukachan Exp $
+#
+
+package FML::Command::signoff;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+use FML::Command::unsubscribe;
+@ISA = qw(FML::Command::unsubscribe);
+
+sub signoff
+{
+    my ($self, $curproc, $args) = @_;
+    $self->SUPER::unsubscribe($curproc, $args);
+}
+
+
+=head1 NAME
+
+FML::Command::signoff - signoff the specified member
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+FML::Command::signoff appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
