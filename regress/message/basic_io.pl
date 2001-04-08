b67709e3c81c2a49ba2180b125d1530e805b3e39
fukachan <fukachan>
2001-04-08 05:05:10 +0000

    dump a list of data_type, show diff

--- regress/message/basic_io.pl
+++ regress/message/basic_io.pl
@@ -5,7 +5,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML: __template.pm,v 1.5 2001/04/03 09:45:39 fukachan Exp $
+# $FML: basic_io.pl,v 1.1.1.1 2001/04/07 12:47:01 fukachan Exp $
 #
 
 use strict;
@@ -13,18 +13,32 @@ use Carp;
 use lib qw(../../cpan/lib);
 use Mail::Message;
 
+my $tmp = "/tmp/buf$$";
+
 for my $f (@ARGV) {
-    my $tmp = "/tmp/buf$$";
     my $fh  = new FileHandle $f;
     my $wh  = new FileHandle "> $tmp";
-    my $obj = Mail::Message->parse( $fh );
+    my $obj = Mail::Message->parse( { fd => $fh } );
 
     $wh->autoflush(1);
     $obj->print($wh);
 
-    print "-- $f ";
-    system "diff -ub $f $tmp";
-    print $@ ? "fail\n": "ok\n";
+    print "\n<< $f\n";
+
+    # use Data::Dumper; print Dumper( $obj );
+
+    my $h = $obj->get_data_type_list;
+    for (@$h) { print "  ", $_, "\n";}
+
+    print "\n";
+    use IO::Handle;
+    my $fd = new IO::Handle;
+    my $i  = 0;
+    open($fd, "diff -ub $f $tmp|");
+    while (<$fd>) { $i++; print "      ", $_ if $i > 3;}
+    close($fd);
 }
+
+unlink $tmp;
 
 exit 0;
