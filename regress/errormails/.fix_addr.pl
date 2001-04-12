d8d9658a431ffd57688c9de3c1e29858216d3701
fukachan <fukachan>
2001-04-12 14:44:22 +0000

    address and domain scrambler

---
+++ regress/errormails/.fix_addr.pl
@@ -0,0 +1,27 @@
+#!/usr/local/bin/perl
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $FML: .fix_addr.pl,v 1.2 2001/04/10 14:34:54 fukachan Exp $
+#
+
+use strict;
+use Carp;
+
+my $base_addr = '10.20.30.';
+my $i         = 1;
+
+while (<>) {
+    # domain 
+    s/sapporo.iij.ad.jp/fml.org/g;
+ 
+    # scramble ip address information  
+    my $addr = $base_addr . $i++;
+    s/(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/$addr/e;
+    print;
+}
+
+exit 0;
