0f51d63d5c7d419ec0041e0595a23456dee27d6e
fukachan <fukachan>
2001-04-13 04:34:07 +0000

    test mode (not test_mode by default) which shows ok / fail

--- regress/message/basic_io.pl
+++ regress/message/basic_io.pl
@@ -5,13 +5,15 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML: basic_io.pl,v 1.2 2001/04/08 05:05:10 fukachan Exp $
+# $FML: basic_io.pl,v 1.3 2001/04/13 04:03:11 fukachan Exp $
 #
 
 use strict;
 use Carp;
 use lib qw(../../cpan/lib);
 use Mail::Message;
+
+my $test_mode = $ENV{'test_mode'} ? 1 : 0;
 
 my $tmp = "/tmp/buf$$";
 
@@ -24,20 +26,23 @@ for my $f (@ARGV) {
     $obj->print($wh);
     $wh->close;
 
-    print "\n<< $f\n";
+    unless ($test_mode) {
+	print "\n<< $f\n";
+	my $h = $obj->get_data_type_list;
+	for (@$h) { print "  ", $_, "\n";}
+	print "\n";
+    }
 
-    # use Data::Dumper; print Dumper( $obj );
-
-    my $h = $obj->get_data_type_list;
-    for (@$h) { print "  ", $_, "\n";}
-
-    print "\n";
     use IO::Handle;
     my $fd = new IO::Handle;
     my $i  = 0;
     open($fd, "diff -ub $f $tmp|");
     while (<$fd>) { $i++; print "      ", $_ if $i > 3;}
     close($fd);
+
+    print "$f ";
+    print $i ? "fail" : "ok";
+    print "\n";
 }
 
 unlink $tmp;
