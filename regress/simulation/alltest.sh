325eea484772229ca0d03ae8832d903b3321dc56
fukachan <fukachan>
2001-04-15 04:59:09 +0000

    use ../message/scramble.pl

--- regress/simulation/alltest.sh
+++ regress/simulation/alltest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: alltest.sh,v 1.4 2001/04/08 07:21:59 fukachan Exp $
 #
 
 (cd ../../fml/etc/;sh .gen.sh)
@@ -8,7 +8,7 @@ sudo rm -f /etc/fml/main.cf
 sudo rm -f /usr/local/libexec/fml/loader
 (cd ../..; sudo sh INSTALL.sh )
 
-cat ../testmails/text=plain |\
+../message/scramble.pl ../testmails/text=plain |\
 perl -w /usr/local/libexec/fml/fml.pl --params pwd=$PWD /var/spool/ml/elena
 	
 exit 0
