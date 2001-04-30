0d57b10f72bf3ab6e1e984e15d759e973ce3d891
fukachan <fukachan>
2001-04-30 10:47:47 +0000

    table-ify tool (useful ??)

---
+++ fml/utils/bin/gen_table.pl
@@ -0,0 +1,44 @@
+#!/usr/local/bin/perl
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $FML$
+#
+
+use strict;
+use Carp;
+use FileHandle;
+use File::Basename;
+
+print "<UL>\n";
+
+for my $f (@ARGV) {
+    my $fn = basename($f);
+    print "\t<LI><A HREF=\"$f\">$fn</A>\n\n";
+
+    my $fh = new FileHandle $f;
+    my $p = 0;
+
+  IN:
+    while (<$fh>) {
+	next if /^\s*$/;
+	last if $_;
+    }
+
+  IN:
+    while (<$fh>) {
+	$p++ if /^\s*$/;
+	
+	print $_ if $p == 2;
+	last IN if $p > 2;
+    } 
+
+    print "\n";
+}
+
+print "</UL>\n";
+
+exit 0;
