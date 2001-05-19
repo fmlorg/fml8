e87fb2090d06de13345ad668477f07b8193c956b
fukachan <fukachan>
2001-05-19 03:40:36 +0000

    pick up queue by Mail::Delivery::Queue and send it sequentially using FML::Mailer

--- fml/lib/FML/Process/QueueManager.pm
+++ fml/lib/FML/Process/QueueManager.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML$
+# $FML: QueueManager.pm,v 1.1 2001/05/18 10:39:29 fukachan Exp $
 #
 
 package FML::Process::QueueManager;
@@ -26,21 +26,77 @@ FML::Process::QueueManager - provide queue manipulation functions
 
 use FML::Log qw(Log LogWarn LogError);
 
+
+sub new
+{
+    my ($self, $args) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = {};
+
+    $me->{ _directory } = $args->{ directory };
+
+    return bless $me, $type;
+}
+
+
 sub send
 {
-    my ($self, $queuefile) = @_;
+    my ($self, $curproc) = @_;
+    my $queue_dir = $self->{ _directory };
+
+    use Mail::Delivery::Queue;
+    my $queue = new Mail::Delivery::Queue { directory => $queue_dir };
+    my $ra    = $queue->list();
+
+    for my $qid (@$ra) { 
+	my $q = new Mail::Delivery::Queue { 
+	    id        => $qid,
+	    directory => $queue_dir,
+	};
+
+	if ( $q->lock() ) {
+	    if ( $q->valid() ) {
+		$self->_send($curproc, $q) && $q->remove();
+	    }
+	    else {
+		Log("$qid is invalid");
+	    }
+	    $q->unlock();
+	}
+	else {
+	    Log("$qid is locked. retry");
+	}
+    }
+}
+
+
+sub _send
+{
+    my ($self, $curproc, $q) = @_;
+    my $info  = $q->getidinfo();
+    my $qfile = $info->{ 'path' };
+    my $qid   = $info->{ 'id' };
 
     use Mail::Message;
-    my $msg = Mail::Message->parse( { file => $queuefile } );
+    my $msg = Mail::Message->parse( { file => $qfile } );
 
     use FML::Mailer;
     my $obj = new FML::Mailer;
-    my ($sender, $rcpt);
-    $obj->send( {
-	sender    => $sender,
-	recipient => $rcpt,
-	message   => $msg,
+    my $r   = $obj->send({
+	sender     => $info->{ sender },
+	recipients => $info->{ recipients },
+	message    => $msg,
+	curproc    => $curproc,
     });
+
+    if ($r) {
+	Log("queue=$qid status=sent");
+    }
+    else {
+	Log("queue=$qid status=fail");
+    }
+
+    return $r;
 }
 
 
