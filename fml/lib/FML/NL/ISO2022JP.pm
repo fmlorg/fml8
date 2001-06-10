9c190b8baf2a5975ae9b77729c09404276773bff
fukachan <fukachan>
2001-06-10 04:11:39 +0000

    Initial revision

---
+++ fml/lib/FML/NL/ISO2022JP.pm
@@ -0,0 +1,74 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $FML: ISO2022JP.pm,v 1.4 2001/05/30 14:35:11 fukachan Exp $
+#
+
+package Language::ISO2022JP;
+
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+
+=head1 NAME
+
+Language::ISO2022JP - adapter for Language::Japanese
+
+=head1 SYNOPSIS
+
+    use Language::ISO2022JP qw(is_iso2022jp_string);
+    if ( is_iso2022jp_string($string) ) { do_something_if_Japanese;}
+
+    use Language::ISO2022JP qw(STR2EUC);
+    $euc_string = STR2EUC( $string );
+
+=cut
+
+
+use Language::Japanese::Utils qw(is_iso2022jp_string);
+use Language::Japanese::String qw(STR2JIS STR2EUC STR2SJIS);
+
+require Exporter;
+@ISA = qw(Language::Japanese::Utils Language::Japanese::String Exporter);
+push(@EXPORT_OK, @Language::Japanese::Utils::EXPORT_OK);
+push(@EXPORT_OK, @Language::Japanese::String::EXPORT_OK);
+
+
+=head1 METHODS
+
+=head2 C<is_iso2022jp_string>
+
+See L<Language::Japanese::Utils>
+
+=head2 C<STR2JIS> C<STR2EUC> C<STR2SJIS>
+
+See L<Language::Japanese::String>
+
+=head1 SEE ALSO
+
+L<Language::Japanese::Utils>,
+L<Language::Japanese::String>
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+Language::ISO2022JP appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+1;
---
+++ fml/lib/FML/NL/Japanese/Utils.pm
@@ -0,0 +1,101 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $FML: Utils.pm,v 1.3 2001/04/03 09:45:48 fukachan Exp $
+#
+
+package Language::Japanese::Utils;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+=head1 NAME
+
+Language::Japanese::Utils - what is this
+
+=head1 SYNOPSIS
+
+   use Language::Japanese::Utils qw(is_iso2022jp_string);
+   if ( is_iso2022jp_string($string) ) { do_something_if_Japanese;}
+
+=head1 DESCRIPTION
+
+Utilities for Japanse string
+
+=head1 METHODS
+
+=head2 <is_iso2022jp_string($string)>
+
+check whether $string looks Japanese. 
+return 1 if it looks so.
+
+=cut
+
+require Exporter;
+
+@ISA       = qw(Exporter);
+@EXPORT_OK = qw(is_iso2022jp_string);
+
+
+sub is_iso2022jp_string
+{
+    my ($buf) = @_;
+    return (not _look_not_iso2022jp_string($buf));
+}
+
+
+# based on fml-support: 07020, 07029
+#   Koji Sudo <koji@cherry.or.jp>
+#   Takahiro Kambe <taca@sky.yamashina.kyoto.jp>
+# check the given buffer has unusual Japanese (not ISO-2022-JP)
+sub _look_not_iso2022jp_string
+{
+    my ($buf) = @_;
+
+    # check 8 bit on
+    if ($buf =~ /[\x80-\xFF]/){
+        return 1;
+    }
+
+    # check SI/SO
+    if ($buf =~ /[\016\017]/) {
+        return 1;
+    }
+
+    # HANKAKU KANA
+    if ($buf =~ /\033\(I/) {
+        return 1;
+    }
+
+    # MSB flag or other control sequences
+    if ($buf =~ /[\001-\007\013\015\020-\032\034-\037\177-\377]/) {
+        return 1;
+    }
+
+    0; # O.K.
+}
+
+
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+Language::Japanese::Utils appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+1;
