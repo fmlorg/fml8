27615774407c06a297099652578e4a4c23ec34c9
fukachan <fukachan>
2001-08-26 08:01:04 +0000

    Initial revision

---
+++ fml/lib/FML/Command/Admin/add.pm
@@ -0,0 +1,54 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $FML: add.pm,v 1.2 2001/08/26 07:59:03 fukachan Exp $
+#
+
+package FML::Command::Admin::add;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+use FML::Command::Utils;
+use FML::Command::Admin::subscribe;
+@ISA = qw(FML::Command::Admin::subscribe FML::Command::Utils);
+
+sub process
+{
+    my ($self, $curproc, $optargs) = @_;
+    $self->SUPER::process($curproc, $optargs);
+}
+
+=head1 NAME
+
+FML::Command::Admin::add - add a new member
+
+=head1 SYNOPSIS
+
+See C<FML::Command> for more details.
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+FML::Command::Admin::add appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
---
+++ fml/lib/FML/Command/Admin/resign.pm
@@ -0,0 +1,56 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $FML: resign.pm,v 1.2 2001/08/26 07:59:03 fukachan Exp $
+#
+
+package FML::Command::Admin::resign;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+use FML::Command::Utils;
+use FML::Command::Admin::unsubscribe;
+@ISA = qw(FML::Command::Admin::unsubscribe use FML::Command::Utils);
+
+sub process
+{
+    my ($self, $curproc, $optargs) = @_;
+    $self->unsubscribe($curproc, $optargs);
+}
+
+=head1 NAME
+
+FML::Command::Admin::resign - alias of "unsubscribe" command
+
+=head1 SYNOPSIS
+
+See C<FML::Command> for more details.
+
+=head1 DESCRIPTION
+
+all requests are forwarded to C<FML::Command::Admin::unsubscribe>.
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+FML::Command::Admin::resign appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
