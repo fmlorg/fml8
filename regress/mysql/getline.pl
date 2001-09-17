7a092f547e52799b137018d03bcbf22b5eae8883
fukachan <fukachan>
2001-09-17 11:42:03 +0000

    modify parameters to sync w/ my environment. more debug info

--- regress/mysql/getline.pl
+++ regress/mysql/getline.pl
@@ -1,6 +1,6 @@
 #!/usr/pkg/bin/perl
 #
-# $FML: getline.pl,v 1.8 2001/05/04 14:32:36 fukachan Exp $
+# $FML: getline.pl,v 1.9 2001/06/17 09:00:31 fukachan Exp $
 #
 
 use strict;
@@ -12,10 +12,10 @@ my $map = 'mysql:toymodel';
 
 my $map_params = {
     $map => {
-	sql_server    => 'localhost',
+	sql_server    => 'mysql.home.fml.org',
 	user          => 'fukachan',
 	user_password => 'uja',
-	database      => 'fml',
+	database      => 'ML',
 	table         => 'ml',
 	params        => {
 	    ml_name   => 'elena',
@@ -27,14 +27,21 @@ my $map_params = {
 
 my $obj = new IO::Adapter ($map, $map_params);
 
+unless (defined $obj) {
+   croak "cannot set up $map\n";
+}
+
+print STDERR "* current table\n";
 _dump($obj); print "\n";
 
+print STDERR "* add rudo\@nuinui.net into current table\n";
 $obj->open();
 $obj->add( 'rudo@nuinui.net' );
 $obj->close();
 
 _dump($obj); print "\n";
 
+print STDERR "* delete rudo\@nuinui.net from current table\n";
 $obj->open();
 $obj->delete( 'rudo@nuinui.net' );
 $obj->close();
@@ -51,6 +58,8 @@ sub _dump
     if (defined $obj) {
 	if (defined $obj->open()) {
 	    while ($_ = $obj->getline()) {
+		my $y = $obj->eof ? "y" : "n";
+		print "<", $obj->getpos, "(eof=$y)> ";
 		print $_, "\n";
 	    }
 	    $obj->close;
