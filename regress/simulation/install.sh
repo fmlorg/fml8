83160d053eb74c54de051939bebce00f7b54438b
fukachan <fukachan>
2001-09-22 14:10:05 +0000

    install config.cf

--- regress/simulation/install.sh
+++ regress/simulation/install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: install.sh,v 1.2 2001/04/08 07:21:59 fukachan Exp $
 #
+
+# config.cf
+echo update /var/spool/ml/elena/config.cf
+cp /var/spool/ml/elena/config.cf /var/spool/ml/elena/config.cf.bak
+cp config.cf /var/spool/ml/elena/config.cf
+
 
 (cd ../../fml/etc/;sh .gen.sh)
 sudo rm -f /etc/fml/main.cf 
