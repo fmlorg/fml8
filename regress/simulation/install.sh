f301de76ac9e7539744e3d1de192fb2cfab2fc4b
fukachan <fukachan>
2001-10-08 15:41:16 +0000

    sudo -v before/after main

--- regress/simulation/install.sh
+++ regress/simulation/install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #
-# $FML: install.sh,v 1.2 2001/04/08 07:21:59 fukachan Exp $
+# $FML: install.sh,v 1.3 2001/09/22 14:10:06 fukachan Exp $
 #
 
 # config.cf
+sudo -v
 echo update /var/spool/ml/elena/config.cf
 cp /var/spool/ml/elena/config.cf /var/spool/ml/elena/config.cf.bak
 cp config.cf /var/spool/ml/elena/config.cf
@@ -14,3 +15,4 @@ sudo rm -f /etc/fml/main.cf
 sudo rm -f /usr/local/libexec/fml/loader
 (cd ../..; sudo sh INSTALL.sh )
 
+sudo -v
