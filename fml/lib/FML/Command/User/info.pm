74b3d472e3c2015ec1ffe9712d6ce80a02b3e32f
fukachan <fukachan>
2001-10-17 11:09:19 +0000

    alias of guide command

---
+++ fml/lib/FML/Command/User/info.pm
@@ -0,0 +1,55 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself. 
+#
+# $FML: info.pm,v 1.4 2001/10/17 10:58:11 fukachan Exp $
+#
+
+package FML::Command::User::info;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+use FML::Command::Utils;
+use FML::Command::User::guide;
+@ISA = qw(FML::Command::User::guide FML::Command::Utils);
+
+sub process
+{
+    my ($self, $curproc, $command_args) = @_;
+    $self->SUPER::process($curproc, $command_args);
+}
+
+
+=head1 NAME
+
+FML::Command::User::info - send guide file
+
+=head1 SYNOPSIS
+
+See C<FML::Command> for more details.
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself. 
+
+=head1 HISTORY
+
+FML::Command::User::info appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
