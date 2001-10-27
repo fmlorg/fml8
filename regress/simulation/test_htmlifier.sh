4ceed46d1846dcb511de66296e85f43e1254365d
fukachan <fukachan>
2001-10-27 04:28:35 +0000

    htmlifier emulator

---
+++ regress/simulation/test_htmlifier.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./perlcheck HTML/Lite.pm ||exit 1
+
+x=spool/? 
+
+rm -fr /tmp/htdocs 
+mkdir /tmp/htdocs 
+perl -I ../../cpan/lib Mail/HTML/Lite.pm \
+	/bakfs/project/fml/fml/ml/fml-help/spool
+ls -lRa /tmp/htdocs 
+# cat /tmp/htdocs/msg2.html 
+# apply readdb.pl /tmp/htdocs/.ht_mhl_*db
