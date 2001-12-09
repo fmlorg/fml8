dacc9759399efd1cf408695b3fcef073d16ace83
fukachan <fukachan>
2001-12-09 14:19:32 +0000

    ensure o+w before processing

--- Documentation/en/I-D/.update.sh
+++ Documentation/en/I-D/.update.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: .update.sh,v 1.1 2001/11/17 11:36:45 fukachan Exp $
 #
 
+chmod 644 draft-*
 
 apply echo draft-*txt | sed 's@\-[0-9][0-9].txt@@' | while read file
 do
