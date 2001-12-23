7f625742e5c3fbbd217a1ed2832b683aa1690729
fukachan <fukachan>
2001-12-23 03:01:27 +0000

    FML::Checksum -> Mail::Message::Checksum

--- regress/base/md5.pl
+++ regress/base/md5.pl
@@ -5,7 +5,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML: md5.pl,v 1.3 2001/04/03 09:51:12 fukachan Exp $
+# $FML: md5.pl,v 1.4 2001/06/17 09:00:29 fukachan Exp $
 #
 
 $file = shift || '/etc/group';
@@ -17,13 +17,13 @@ my $fh = new FileHandle $file;
 while (<$fh>) { $body .=  $_;}
 close($fh);
 
-use FML::Checksum;
-$p = new FML::Checksum;
+use Mail::Message::Checksum;
+$p = new Mail::Message::Checksum;
 
 $internal = $p->md5( \$body );
 $external = program($file);
 
-print "FML::Checksum::md5 ... ";
+print "Mail::Message::Checksum::md5 ... ";
 print (($internal eq $external) ? "ok" : "fail");
 print "\n";
 
