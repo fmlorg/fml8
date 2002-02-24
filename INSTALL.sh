94225e7eb58c3a18271f02802bd07a71f98f9234
fukachan <fukachan>
2002-02-24 08:16:00 +0000

    install aliases as a temlate for "makefml newml" chmod 644 include include-ctl aliases

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML: INSTALL.sh,v 1.40 2001/12/11 12:13:48 fukachan Exp $
+# $FML: INSTALL.sh,v 1.41 2001/12/16 13:33:29 fukachan Exp $
 #
 
 # Run this from the top-level fml source directory.
@@ -66,9 +66,10 @@ echo updating $default_config_dir/
 cp fml/etc/default_config.cf.ja $default_config_dir/default_config.cf
 cp fml/etc/config.cf.ja         $default_config_dir/config.cf
 
-for file in include include-ctl
+for file in include include-ctl aliases
 do
    cp fml/etc/$file $default_config_dir/$file
+   chmod 644 $default_config_dir/$file
 done
 
 echo updating $lib_dir/$fml_version/
