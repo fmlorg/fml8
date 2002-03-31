1c1c1e15ebf6a63c9895990597d7e9f28c907048
fukachan <fukachan>
2002-03-31 05:25:47 +0000

    use Mail::Message::ToHTML

--- regress/simulation/test_htmlifier.sh
+++ regress/simulation/test_htmlifier.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #
-# $FML: test_htmlifier.sh,v 1.2 2001/10/27 04:52:02 fukachan Exp $
+# $FML: test_htmlifier.sh,v 1.3 2001/10/27 16:44:19 fukachan Exp $
 #
 
-./perlcheck HTML/Lite.pm ||exit 1
+./perlcheck Mail/Message/ToHTML.pm ||exit 1
 
 spool_dir=${spool_dir:-/bakfs/project/fml/fml/ml/fml-help/spool}
 
 rm -fr /tmp/htdocs 
 mkdir /tmp/htdocs 
 
-perl -I ../../cpan/lib Mail/HTML/Lite.pm $spool_dir
+perl -I ../../cpan/lib Mail/Message/ToHTML.pm $spool_dir
 
 ls -lRa /tmp/htdocs 
 # cat /tmp/htdocs/msg2.html 
