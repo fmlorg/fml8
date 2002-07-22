b18c3a0f0927b6b9c92ab53f2bedaee3df7388c0
fukachan <fukachan>
2002-07-22 11:34:09 +0000

    allow from *

--- regress/mysql/adduser.sql
+++ regress/mysql/adduser.sql
@@ -1,2 +1,2 @@
 INSERT INTO user
-VALUES ('localhost','fukachan',Password('uja'),'Y','Y','Y','Y','Y','N','N','N','N','N','N','N','N','N');
+VALUES ('%','fml',Password('uja'),'Y','Y','Y','Y','Y','N','N','N','N','N','N','N','N','N');
