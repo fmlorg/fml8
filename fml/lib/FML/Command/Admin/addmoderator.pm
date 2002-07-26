dfdbb43e2016843fb5f4f8b79560ce081ec4bb23
fukachan <fukachan>
2002-07-26 00:40:56 +0000

    add/del moderator in {members,recipients}-moderator

---
+++ fml/lib/FML/Command/Admin/addmoderator.pm
@@ -0,0 +1,131 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2002 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself.
+#
+# $FML: addmoderator.pm,v 1.5 2002/07/25 15:40:58 fukachan Exp $
+#
+
+package FML::Command::Admin::addmoderator;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+
+=head1 NAME
+
+FML::Command::Admin::addmoderator - add a new moderator
+
+=head1 SYNOPSIS
+
+See C<FML::Command> for more details.
+
+=head1 DESCRIPTION
+
+add a new moderator address.
+
+=head1 METHODS
+
+=head2 C<process($curproc, $command_args)>
+
+=cut
+
+
+# Descriptions: standard constructor
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: OBJ
+sub new
+{
+    my ($self) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = {};
+    return bless $me, $type;
+}
+
+
+# Descriptions: need lock or not
+#    Arguments: none
+# Side Effects: none
+# Return Value: NUM( 1 or 0)
+sub need_lock { 1;}
+
+
+# Descriptions: addmoderator a new user
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_args)
+# Side Effects: update $member_map $recipient_map
+# Return Value: none
+sub process
+{
+    my ($self, $curproc, $command_args) = @_;
+    my $config        = $curproc->{ config };
+    my $member_map    = $config->{ primary_moderator_member_map };
+    my $recipient_map = $config->{ primary_moderator_recipient_map };
+    my $options       = $command_args->{ options };
+    my $address       = $command_args->{ command_data } || $options->[ 0 ];
+
+    # fundamental check
+    croak("address is not undefined")    unless defined $address;
+    croak("member_map is not undefined") unless defined $member_map;
+    croak("address is not specified")    unless $address;
+    croak("member_map is not specified") unless $member_map;
+
+    # FML::Command::UserControl specific parameters
+    my $uc_args = {
+	address => $address,
+	maplist => [ $member_map, $recipient_map ],
+    };
+    my $r = '';
+
+    eval q{
+	use FML::Command::UserControl;
+	my $obj = new FML::Command::UserControl;
+	$obj->useradd($curproc, $command_args, $uc_args);
+    };
+    if ($r = $@) {
+	croak($r);
+    }
+}
+
+
+# Descriptions: cgi menu to add a new user
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_args)
+# Side Effects: update $member_map $recipient_map
+# Return Value: none
+sub cgi_menu
+{
+    my ($self, $curproc, $args, $command_args) = @_;
+    my $r = '';
+
+    eval q{
+	use FML::CGI::Admin::User;
+	my $obj = new FML::CGI::Admin::User;
+	$obj->cgi_menu($curproc, $args, $command_args);
+    };
+    if ($r = $@) {
+	croak($r);
+    }
+}
+
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2002 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 HISTORY
+
+FML::Command::Admin::addmoderator appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
---
+++ fml/lib/FML/Command/Admin/delmoderator.pm
@@ -0,0 +1,131 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2001,2002 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself.
+#
+# $FML: delmoderator.pm,v 1.1 2002/07/13 09:45:41 fukachan Exp $
+#
+
+package FML::Command::Admin::delmoderator;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+
+=head1 NAME
+
+FML::Command::Admin::delmoderator - remove the specified moderator
+
+=head1 SYNOPSIS
+
+See C<FML::Command> for more details.
+
+=head1 DESCRIPTION
+
+delmoderator a new user.
+
+=head1 METHODS
+
+=head2 C<process($curproc, $command_args)>
+
+=cut
+
+
+# Descriptions: standard constructor
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: OBJ
+sub new
+{
+    my ($self) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = {};
+    return bless $me, $type;
+}
+
+
+# Descriptions: need lock or not
+#    Arguments: none
+# Side Effects: none
+# Return Value: NUM( 1 or 0)
+sub need_lock { 1;}
+
+
+# Descriptions: remove the specified moderator
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_args)
+# Side Effects: update $member_map $recipient_map
+# Return Value: none
+sub process
+{
+    my ($self, $curproc, $command_args) = @_;
+    my $config        = $curproc->{ config };
+    my $member_map    = $config->{ primary_moderator_member_map };
+    my $recipient_map = $config->{ primary_moderator_recipient_map };
+    my $options       = $command_args->{ options };
+    my $address       = $command_args->{ command_data } || $options->[ 0 ];
+
+    # fundamental check
+    croak("address is not undefined")    unless defined $address;
+    croak("member_map is not undefined") unless defined $member_map;
+    croak("address is not specified")    unless $address;
+    croak("member_map is not specified") unless $member_map;
+
+    # FML::Command::UserControl specific parameters
+    my $uc_args = {
+	address => $address,
+	maplist => [ $member_map, $recipient_map ],
+    };
+    my $r = '';
+
+    eval q{
+	use FML::Command::UserControl;
+	my $obj = new FML::Command::UserControl;
+	$obj->userdel($curproc, $command_args, $uc_args);
+    };
+    if ($r = $@) {
+	croak($r);
+    }
+}
+
+
+# Descriptions: show cgi menu to remove the moderator
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_args)
+# Side Effects: update $member_map $recipient_map
+# Return Value: none
+sub cgi_menu
+{
+    my ($self, $curproc, $args, $command_args) = @_;
+    my $r = '';
+
+    eval q{
+	use FML::CGI::Admin::User;
+	my $obj = new FML::CGI::Admin::User;
+	$obj->cgi_menu($curproc, $args, $command_args);
+    };
+    if ($r = $@) {
+	croak($r);
+    }
+}
+
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2001,2002 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 HISTORY
+
+FML::Command::Admin::delmoderator appeared in fml5 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
