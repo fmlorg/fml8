012c66047885129ac773360f9cce9df2f9e24948
tmu <tmu>
2002-09-13 16:35:57 +0000

    add use Mail::Message::Encode at sub reject_japanese_command_syntax because rejected by filter due to Undefined subroutine &FML::Filter::BodyCheck::STR2EUC called

--- fml/lib/FML/Filter/BodyCheck.pm
+++ fml/lib/FML/Filter/BodyCheck.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: BodyCheck.pm,v 1.22 2002/08/16 15:59:19 fukachan Exp $
+# $FML: BodyCheck.pm,v 1.23 2002/09/11 23:18:12 fukachan Exp $
 #
 
 package FML::Filter::BodyCheck;
@@ -353,6 +353,7 @@ sub reject_japanese_command_syntax
 	# trap /JIS"2byte"[A-Za-z]+/
 
 	# EUC-fy for further investigation
+	use Mail::Message::Encode qw(STR2EUC);
 	my $s = &STR2EUC($buf);
 	$s    = (split(/\n/, $s))[0]; # check the first line only
 
