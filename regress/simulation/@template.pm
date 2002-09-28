c286560bb9160392580ec060ee783ad6aeca9aa1
fukachan <fukachan>
2002-09-28 05:36:03 +0000

    fml 5 -> fml 8

--- regress/simulation/@template.pm
+++ regress/simulation/@template.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: @template.pm,v 1.4 2001/12/23 12:25:44 fukachan Exp $
+# $FML: @template.pm,v 1.5 2002/01/18 15:37:38 fukachan Exp $
 #
 
 package __MODULE_NAME__;
@@ -52,7 +52,7 @@ redistribute it and/or modify it under the same terms as Perl itself.
 
 =head1 HISTORY
 
-__MODULE_NAME__ appeared in fml5 mailing list driver package.
+__MODULE_NAME__ appeared in fml8 mailing list driver package.
 See C<http://www.fml.org/> for more details.
 
 =cut
