7e697cb1ae29ae8c5b141935fe4c14029372ba8a
fukachan <fukachan>
2002-11-23 15:20:00 +0000

    basic_variables() -> basic_variable() for FML::Restriction::Base

--- fml/lib/FML/Command/Admin/dir.pm
+++ fml/lib/FML/Command/Admin/dir.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: dir.pm,v 1.5 2002/09/11 23:18:07 fukachan Exp $
+# $FML: dir.pm,v 1.6 2002/09/22 14:56:44 fukachan Exp $
 #
 
 package FML::Command::Admin::dir;
@@ -67,7 +67,7 @@ sub process
 
     use FML::Restriction::Base;
     my $safe   = new FML::Restriction::Base;
-    my $regexp = $safe->basic_variables();
+    my $regexp = $safe->basic_variable();
     my $dirreg = $regexp->{ directory };
 
     # analyze ...
--- fml/lib/FML/Command/Admin/file.pm
+++ fml/lib/FML/Command/Admin/file.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: file.pm,v 1.5 2002/09/11 23:18:07 fukachan Exp $
+# $FML: file.pm,v 1.6 2002/09/22 14:56:45 fukachan Exp $
 #
 
 package FML::Command::Admin::file;
@@ -74,7 +74,7 @@ sub process
 
     use FML::Restriction::Base;
     my $safe    = new FML::Restriction::Base;
-    my $regexp  = $safe->basic_variables();
+    my $regexp  = $safe->basic_variable();
     my $filereg = $regexp->{ file };
 
     # argv = command subcommand args ...
