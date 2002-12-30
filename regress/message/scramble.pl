4c45aa65babaa120140169c6863fcbb723df643c
fukachan <fukachan>
2002-12-30 13:18:50 +0000

    if envvar FML_EMUL_FROM defined, rewrite From: field to the specified value.

--- regress/message/scramble.pl
+++ regress/message/scramble.pl
@@ -1,21 +1,35 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 #-*- perl -*-
 #
-#  Copyright (C) 2001 Ken'ichi Fukamachi
+#  Copyright (C) 2001,2002 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML: __template.pm,v 1.5 2001/04/03 09:45:39 fukachan Exp $
+# $FML: scramble.pl,v 1.1 2001/04/14 14:35:08 fukachan Exp $
 #
 
+use strict;
+use Carp;
+
+my $from = undef;
+my $from_found = 0;
+
+if (defined $ENV{ FML_EMUL_FROM }) {
+    $from = $ENV{ FML_EMUL_FROM };
+} 
 
 while (<>) {
-   if (/message-id/i) {
+    if (/message-id/i) {
 	my $time = time;
 	s/\d+/$time.$$/g;
-   }
+    }
 
-   print ;
+    if (defined($from) && /^From:/ && (not $from_found)) {
+	s/\S+\@\S+/$from/;
+	$from_found = 1;
+    }
+
+    print $_;
 }
 
 exit 0;
