0f5af05553083e8c63423479cf8a7b6bb23fc3cc
fukachan <fukachan>
2003-01-03 06:45:45 +0000

    dummy module to implement rewrite_prompt() for the case of auth fail.

---
+++ fml/lib/FML/Command/User/admin.pm
@@ -0,0 +1,79 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2003 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself.
+#
+# $FML$
+#
+
+package FML::Command::User::admin;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+
+use FML::Command::Admin::password;
+
+
+# Descriptions: constructor.
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: OBJ
+sub new
+{
+    my ($self) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = {};
+    return bless $me, $type;
+}
+
+
+# Descriptions: rewrite buffer to hide the password phrase in $rbuf
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_args) STR_REF($rbuf)
+# Side Effects: none
+# Return Value: none
+sub rewrite_prompt
+{
+    my ($self, $curproc, $command_args, $rbuf) = @_;	
+    my $obj = new FML::Command::Admin::password;
+    $obj->rewrite_prompt($curproc, $command_args, $rbuf);
+}
+
+
+=head1 NAME
+
+FML::Command::User::admin - dummy but provide utility for the case auth fail.
+
+=head1 SYNOPSIS
+
+See C<FML::Command> for more details.
+
+=head1 DESCRIPTION
+
+forward request to C<FML::Command::User::subscribe> module.
+
+=head1 CODING STYLE
+
+See C<http://www.fml.org/software/FNF/> on fml coding style guide.
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2003 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 HISTORY
+
+FML::Command::User::add first appeared in fml8 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
