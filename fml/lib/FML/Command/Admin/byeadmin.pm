974b7927e1f3cda9d2951f407caf2445b7593e08
fukachan <fukachan>
2003-01-27 04:36:29 +0000

    mv real codes from byeadmin to deladmin module. now, byeadmin is a forwarder to deladmin.

--- fml/lib/FML/Command/Admin/byeadmin.pm
+++ fml/lib/FML/Command/Admin/byeadmin.pm
@@ -4,13 +4,27 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: byeadmin.pm,v 1.9 2003/01/25 12:48:37 fukachan Exp $
+# $FML: byeadmin.pm,v 1.10 2003/01/26 03:15:10 fukachan Exp $
 #
 
 package FML::Command::Admin::byeadmin;
 use strict;
 use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
 use Carp;
+
+use FML::Command::Admin::deladmin;
+@ISA = qw(FML::Command::Admin::deladmin);
+
+
+# Descriptions: remove the specified user.
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_args)
+# Side Effects: forward request to deladmin module
+# Return Value: none
+sub process
+{
+    my ($self, $curproc, $command_args) = @_;
+    $self->SUPER::process($curproc, $command_args);
+}
 
 
 =head1 NAME
@@ -28,96 +42,6 @@ remove the specified administrator.
 =head1 METHODS
 
 =head2 C<process($curproc, $command_args)>
-
-=cut
-
-
-# Descriptions: standard constructor
-#    Arguments: OBJ($self)
-# Side Effects: none
-# Return Value: OBJ
-sub new
-{
-    my ($self) = @_;
-    my ($type) = ref($self) || $self;
-    my $me     = {};
-    return bless $me, $type;
-}
-
-
-# Descriptions: need lock or not
-#    Arguments: none
-# Side Effects: none
-# Return Value: NUM( 1 or 0)
-sub need_lock { 1;}
-
-
-# Descriptions: remove the specified administrator
-#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_args)
-# Side Effects: update $member_map $recipient_map
-# Return Value: none
-sub process
-{
-    my ($self, $curproc, $command_args) = @_;
-    my $config = $curproc->config();
-
-    # target maps
-    my $member_maps    = $config->get_as_array_ref('admin_member_maps');
-    my $recipient_maps = $config->get_as_array_ref('admin_recipient_maps');
-    my $options        = $command_args->{ options };
-    my $address        = $command_args->{ command_data } || $options->[ 0 ];
-
-    # fundamental check
-    croak("address not undefined")        unless defined $address;
-    croak("address not specified")        unless $address;
-    croak("member_maps not undefined")    unless defined $member_maps;
-    croak("member_maps not specified")    unless $member_maps;
-    croak("recipient_maps not undefined") unless defined $recipient_maps;
-    croak("recipient_maps not specified") unless $recipient_maps;
-
-    # maplist
-    my $maplist = [];
-    push(@$maplist, @$member_maps)    if @$member_maps;
-    push(@$maplist, @$recipient_maps) if @$recipient_maps;
-
-    # FML::Command::UserControl specific parameters
-    my $uc_args = {
-	address => $address,
-	maplist => $maplist,
-    };
-    my $r = '';
-
-    eval q{
-	use FML::Command::UserControl;
-	my $obj = new FML::Command::UserControl;
-	$obj->userdel($curproc, $command_args, $uc_args);
-    };
-    if ($r = $@) {
-	croak($r);
-    }
-}
-
-
-# Descriptions: show cgi menu to remove the administrator
-#    Arguments: OBJ($self)
-#               OBJ($curproc) HASH_REF($args) HASH_REF($command_args)
-# Side Effects: update $member_map $recipient_map
-# Return Value: none
-sub cgi_menu
-{
-    my ($self, $curproc, $args, $command_args) = @_;
-    my $r = '';
-
-    eval q{
-	use FML::CGI::Admin::User;
-	my $obj = new FML::CGI::Admin::User;
-	$obj->cgi_menu($curproc, $args, $command_args);
-    };
-    if ($r = $@) {
-	croak($r);
-    }
-}
-
 
 =head1 CODING STYLE
 
