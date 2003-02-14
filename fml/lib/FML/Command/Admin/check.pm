b7c1fd619342331cd5d053465e7269294eeeec2b
fukachan <fukachan>
2003-02-14 15:29:02 +0000

    fix copyright

--- fml/lib/FML/Command/Admin/check.pm
+++ fml/lib/FML/Command/Admin/check.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2002 Ken'ichi Fukamachi
+#  Copyright (C) 2002,2003 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: check.pm,v 1.6 2002/12/15 13:46:28 fukachan Exp $
+# $FML: check.pm,v 1.7 2003/02/13 14:40:43 fukachan Exp $
 #
 
 package FML::Command::Admin::check;
@@ -204,7 +204,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2002 Ken'ichi Fukamachi
+Copyright (C) 2002,2003 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
