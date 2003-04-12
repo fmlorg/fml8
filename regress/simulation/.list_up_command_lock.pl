380ed4eaac0518cadb42ac39862735e2d55a6dad
fukachan <fukachan>
2003-04-12 14:02:35 +0000

    tool to list up lock channel used in commands

---
+++ regress/simulation/.list_up_command_lock.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+#
+# $FML$
+#
+
+for my $x (<AdminCommand/*pm UserCommand/*pm>) {
+    _check($x);
+}
+
+exit 0;
+
+
+sub _check
+{
+    my ($file) = @_;
+    my $need_lock = 0;
+    my $channel   = '';
+    my $isa = '';
+
+    use FileHandle;
+    my $fh = new FileHandle $file;
+    if (defined $fh) {
+	while (<$fh>) { 
+	    chomp;
+
+	    if (/\@ISA\s*=\s*qw\(([A-Za-z:]+)/) {
+		$isa = $1;
+		$isa =~ s/FML::Command:://;
+	    }
+
+	    if (/sub need_lock.*1/) {
+		$need_lock = 1;
+	    }
+
+	    if (/sub lock_channel.*return\s*[\'\"](\S+)[\'\"]/) {
+		$channel = $1;
+	    }
+
+	}
+
+	$fh->close();
+    }
+
+    if ($isa) {
+	printf "%-30s  %3s   %s\n", $file, "==>", $isa;
+    }
+    else {
+	printf 
+	    "%-30s  %3s   %s\n", 
+	    $file, 
+	    ($need_lock ? "yes" : "no"), 
+	    $channel;
+    }
+}
