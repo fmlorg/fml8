071445b25b9522ae289f4b96f3fb34e4caafeace
fukachan <fukachan>
2003-07-24 15:37:33 +0000

    translation. (partly submitted, phase 1)

---
+++ fml/doc/en/tutorial/.fix_charset.pl
@@ -0,0 +1,19 @@
+#!/usr/local/bin/perl
+#
+# $FML$
+# $jaFML: .fix_charset.pl,v 1.1 2001/04/23 17:15:52 fukachan Exp $
+#
+
+use strict;
+
+my $meta = q#
+<META http-equiv="Content-Type"
+        content="text/html; charset=EUC-JP">
+#;
+
+while (<>) {
+	s/<META/${meta}$&/;
+	print;
+}
+
+exit 0;
