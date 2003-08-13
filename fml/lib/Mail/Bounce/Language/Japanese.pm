4d50af883af1d5774fdb8ba432fe42e26d90268e
fukachan <fukachan>
2003-08-13 13:43:46 +0000

    fix copyright

--- fml/lib/Mail/Bounce/Language/Japanese.pm
+++ fml/lib/Mail/Bounce/Language/Japanese.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2001,2002 Ken'ichi Fukamachi
+#  Copyright (C) 2001,2002,2003 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Japanese.pm,v 1.7 2002/09/22 14:57:02 fukachan Exp $
+# $FML: Japanese.pm,v 1.8 2003/08/06 11:20:06 fukachan Exp $
 #
 
 #
@@ -132,7 +132,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2001,2002 Ken'ichi Fukamachi
+Copyright (C) 2001,2002,2003 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
