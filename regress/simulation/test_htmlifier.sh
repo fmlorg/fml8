d844753a0fe3cc2925d015bb8d9398b6021ec18e
fukachan <fukachan>
2003-09-19 13:26:11 +0000

    fix @INC

--- regress/simulation/test_htmlifier.sh
+++ regress/simulation/test_htmlifier.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FML: test_htmlifier.sh,v 1.4 2002/03/31 05:25:47 fukachan Exp $
+# $FML: test_htmlifier.sh,v 1.5 2003/08/09 01:39:54 fukachan Exp $
 #
 
 ./perlcheck Mail/Message/ToHTML.pm ||exit 1
@@ -10,7 +10,8 @@ spool_dir=${spool_dir:-/bakfs/project/fml/fml/ml/fml-help/spool}
 rm -fr /tmp/htdocs /tmp/elena
 mkdir /tmp/htdocs 
 
-perl -I ../../cpan/lib Mail/Message/ToHTML.pm $spool_dir
+perl -I ../../fml/lib -I ../../img/lib -I ../../cpan/lib \
+	Mail/Message/ToHTML.pm $spool_dir
 
 ls -lRa /tmp/htdocs 
 # cat /tmp/htdocs/msg2.html 
