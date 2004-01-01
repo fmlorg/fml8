182d9ae3bf62ee6fb6316190b3758f874b725c00
fukachan <fukachan>
2004-01-01 08:41:09 +0000

    fix: cgi_var_cgi_mode() return null string by default.

--- fml/lib/FML/Process/CGI/Utils.pm
+++ fml/lib/FML/Process/CGI/Utils.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Utils.pm,v 1.7 2003/10/16 14:06:57 fukachan Exp $
+# $FML: Utils.pm,v 1.8 2003/10/17 08:16:27 fukachan Exp $
 #
 
 package FML::Process::CGI::Utils;
@@ -179,7 +179,7 @@ sub cgi_var_cgi_mode
     my ($curproc) = @_;
     my $hints     = $curproc->hints();
 
-    return $hints->{ cgi_mode };
+    return( $hints->{ cgi_mode } || '' );
 }
 
 
