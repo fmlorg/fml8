058690631349f815222c5b5acd38f29dca6b193f
fukachan <fukachan>
2004-01-18 14:05:05 +0000

    add comments. FNF: clean up style.

--- fml/lib/FML/CGI/List.pm
+++ fml/lib/FML/CGI/List.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: List.pm,v 1.8 2004/01/01 23:52:09 fukachan Exp $
+# $FML: List.pm,v 1.9 2004/01/02 14:50:29 fukachan Exp $
 #
 
 package FML::CGI::List;
@@ -44,11 +44,13 @@ sub cgi_menu
     use FML::Restriction::Base;
     my $safe = new FML::Restriction::Base;
     unless ($safe->regexp_match('ml_name', $ml_name)) {
+	# XXX-TODO: nl ?
 	croak("invalid ml_name");
     }
 
     # check $map_default included in $map_list.
-    unless ($config->has_attribute("cgi_menu_address_map_list", $map_default)) {
+    unless ($config->has_attribute("cgi_menu_address_map_list", 
+				   $map_default)) {
 	croak("invalid map: $map_default");
     }
 
