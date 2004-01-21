0692487963c45df904fe56330834013d587ebacc
fukachan <fukachan>
2004-01-21 03:41:27 +0000

    define $number_regexp , $address_regexp. modified to use it.

--- fml/lib/FML/Restriction/Base.pm
+++ fml/lib/FML/Restriction/Base.pm
@@ -3,7 +3,7 @@
 # Copyright (C) 2001,2002,2003,2004 Ken'ichi Fukamachi
 #          All rights reserved.
 #
-# $FML: Base.pm,v 1.25 2003/10/15 01:03:36 fukachan Exp $
+# $FML: Base.pm,v 1.26 2004/01/02 14:42:44 fukachan Exp $
 #
 
 package FML::Restriction::Base;
@@ -94,12 +94,14 @@ my $command_regexp = '[-A-Za-z0-9_]+';
 my $file_regexp    = '[-A-Za-z0-9_]+';
 my $dir_regexp     = '[-A-Za-z0-9_]+';
 my $option_regexp  = '[-A-Za-z0-9]+';
+my $number_regexp  = '\d+';
+my $address_regexp = sprintf("%s\@%s", $user_regexp, $domain_regexp);
 my %basic_variable =
     (
      # address, user and domain et.al.
-     'address'           => $user_regexp.'\@'.$domain_regexp,
-     'address_specified' => $user_regexp.'\@'.$domain_regexp,
-     'address_selected'  => $user_regexp.'\@'.$domain_regexp,
+     'address'           => $address_regexp,
+     'address_specified' => $address_regexp,
+     'address_selected'  => $address_regexp,
      'domain'            => $domain_regexp,
      'user'              => $user_regexp,
      'ml_name'           => $user_regexp,
@@ -109,7 +111,7 @@ my %basic_variable =
      'action'            => $command_regexp,
      'command'           => $command_regexp,
      'navi_command'      => $command_regexp,
-     'article_id'        => '\d+',
+     'article_id'        => $number_regexp,
 
      # file, directory et.al.
      'directory'         => $dir_regexp,
@@ -122,6 +124,11 @@ my %basic_variable =
      # misc
      'language'          => $option_regexp,
      );
+
+
+#
+# XXX-TODO: remove methods except for regexp_match() ???
+#
 
 
 =head2 basic_variable()
