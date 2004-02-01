b562d181a89e15e23c4b57facec57d12305e796e
fukachan <fukachan>
2004-02-01 14:35:33 +0000

    fix copyright

--- fml/lib/FML/Error.pm
+++ fml/lib/FML/Error.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2002,2003 Ken'ichi Fukamachi
+#  Copyright (C) 2002,2003,2004 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Error.pm,v 1.27 2003/12/31 03:59:28 fukachan Exp $
+# $FML: Error.pm,v 1.28 2004/01/31 08:12:11 fukachan Exp $
 #
 
 package FML::Error;
@@ -477,7 +477,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2002,2003 Ken'ichi Fukamachi
+Copyright (C) 2002,2003,2004 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
