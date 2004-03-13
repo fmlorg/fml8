6584cfe138fe04e38e15b65534f171675e3fdb61
fukachan <fukachan>
2004-03-13 03:24:03 +0000

    catch up variable renaming

--- fml/utils/bin/listup_commnads.sh
+++ fml/utils/bin/listup_commnads.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: listup_commnads.sh,v 1.1 2002/09/29 06:06:21 fukachan Exp $
 #
 
 cd `dirname $0`/../../.. || exit 1
@@ -9,20 +9,25 @@ test -d fml/doc/share/config  || mkdir -p fml/doc/share/config
 
 echo generating user command list 1>&2
 (
-	fmlconf __default__ commands_for_user | sed 's/.*= *//'
-	fmlconf __default__ commands_for_stranger  | sed 's/.*= *//'
+	fmlconf __default__ user_command_mail_allowed_commands |\
+	sed 's/.*= *//'
+
+	fmlconf __default__ anonymous_command_mail_allowed_commands  |\
+	sed 's/.*= *//'
 ) |\
 tr ' ' '\012' | sort | uniq > fml/doc/share/config/list_user_command.txt
 
 echo generating admin command list 1>&2
 (
-	fmlconf __default__ commands_for_privileged_user | sed 's/.*= *//'
+	fmlconf __default__ admin_command_mail_allowed_commands |\
+	sed 's/.*= *//'
 ) |\
 tr ' ' '\012' | sort | uniq > fml/doc/share/config/list_admin_command.txt
 
 echo generating admin.cgi command list 1>&2
 (
-	fmlconf __default__ commands_for_admin_cgi | sed 's/.*= *//'
+	fmlconf __default__ admin_cgi_allowed_commands |\
+	sed 's/.*= *//'
 )|\
 tr ' ' '\012' | sort | uniq > fml/doc/share/config/list_admin_cgi_command.txt
 
