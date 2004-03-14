e9b78e850b29b4cc78215d76411c3021468b81fd
fukachan <fukachan>
2004-03-14 06:49:26 +0000

    modified to use restriction_state_*(). move permit_anonymous_command_mail() to FML::Restriction::Command.

--- fml/lib/FML/Restriction/Post.pm
+++ fml/lib/FML/Restriction/Post.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Post.pm,v 1.9 2004/01/21 03:40:44 fukachan Exp $
+# $FML: Post.pm,v 1.10 2004/03/12 11:45:55 fukachan Exp $
 #
 
 package FML::Restriction::Post;
@@ -52,12 +52,11 @@ sub reject_system_special_accounts
     my $curproc = $self->{ _curproc };
     my $cred    = $curproc->{ credential };
     my $match   = $cred->match_system_special_accounts($sender);
-    my $pcb     = $curproc->pcb();
 
     if ($match) {
 	$curproc->log("${rule}: $match matches sender address");
-	unless ($pcb->get("check_restrictions", "deny_reason")) {
-	    $pcb->set("check_restrictions", "deny_reason", $rule);
+	unless ($curproc->restriction_state_get_deny_reason()) {
+	    $curproc->restriction_state_set_deny_reason($rule);
 	}
 	return("matched", "deny");
     }
@@ -99,7 +98,6 @@ sub permit_member_maps
     my ($self, $rule, $sender) = @_;
     my $curproc = $self->{ _curproc };
     my $cred    = $curproc->{ credential };
-    my $pcb     = $curproc->pcb();
 
     # Q: the mail sender is a ML member?
     if ($cred->is_member($sender)) {
@@ -117,40 +115,13 @@ sub permit_member_maps
 	# $curproc->reply_message( "   your address: $sender" );
 
 	# save reason for later use.
-	unless ($pcb->get("check_restrictions", "deny_reason")) {
-	    $pcb->set("check_restrictions", "deny_reason", $rule);
+	unless ($curproc->restriction_state_get_deny_reason()) {
+	    $curproc->restriction_state_set_deny_reason($rule);
 	}
 
 	# XXX "deny ASAP if this method fails." ? NO, wrong!
 	# XXX permit_XXX() allows the trial match of another rules.
 	# return("matched", "deny");
-    }
-
-    return(0, undef);
-}
-
-
-#
-# XXX-TODO: permit_anonymous_command_mail -> permit_anonymous_command ?
-#
-
-
-# Descriptions: permit this command even if $sender is a stranger.
-#    Arguments: OBJ($self) STR($rule) STR($sender)
-# Side Effects: none
-# Return Value: ARRAY(STR, STR)
-sub permit_anonymous_command_mail
-{
-    my ($self, $rule, $sender) = @_;
-    my $curproc = $self->{ _curproc };
-    my $pcb     = $curproc->pcb();
-
-    # XXX-TODO: find_anonymous_command_mail_allowed_commands -> match_anonymous_command ?
-    use FML::Command::DataCheck;
-    my $check = new FML::Command::DataCheck;
-    if ($check->find_anonymous_command_mail_allowed_commands($curproc)) {
-	$curproc->log("$rule matched. accepted.");
-	return("matched", "permit");
     }
 
     return(0, undef);
@@ -165,10 +136,9 @@ sub reject
 {
     my ($self, $rule, $sender) = @_;
     my $curproc = $self->{ _curproc };
-    my $pcb     = $curproc->pcb();
 
-    unless ($pcb->get("check_restrictions", "deny_reason")) {
-	$pcb->set("check_restrictions", "deny_reason", $rule);
+    unless ($curproc->restriction_state_get_deny_reason()) {
+	$curproc->restriction_state_set_deny_reason($rule);
     }
     return("matched", "deny");
 }
