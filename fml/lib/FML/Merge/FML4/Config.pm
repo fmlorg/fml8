2b60e0653cd2f1c4211b2973bf31a802002afc8e
fukachan <fukachan>
2004-03-16 12:58:20 +0000

    Initial revision

---
+++ fml/lib/FML/Merge/FML4/Config.pm
@@ -0,0 +1,266 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2004 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself.
+#
+# $FML: @template.pm,v 1.8 2004/01/01 07:29:27 fukachan Exp $
+#
+
+package FML::Merge::FML4::Config;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+
+=head1 NAME
+
+FML::Merge::FML4::Config - what files we need to merge
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 C<new()>
+
+=cut
+
+
+# Descriptions: constructor.
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($config)
+# Side Effects: none
+# Return Value: OBJ
+sub new
+{
+    my ($self, $curproc, $config) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = {};
+    return bless $me, $type;
+}
+
+
+# target files to back up.
+my $ml_home_dir_backup_target_files = {
+
+    # 
+    # main configuration files
+    # 
+    'cf' => {
+	'backup_mode' => 'move',
+    },
+
+    'config.ph' => {
+	'backup_mode' => 'copy',
+    },
+
+
+    # 
+    # system
+    # 
+    'aliases' => {
+	'backup_mode' => 'move',
+    },
+
+    'crontab' => {
+	'backup_mode' => 'move',
+    },
+
+    'fmlwrapper.c' => {
+	'backup_mode' => 'move',
+    },
+
+    'fmlwrapper.h' => {
+	'backup_mode' => 'move',
+    },
+
+
+    # 
+    # message files
+    # 
+    'confirm' => {
+	'backup_mode' => 'move',
+    },
+
+    'deny' => {
+	'backup_mode' => 'move',
+    },
+
+    'guide' => {
+	'backup_mode' => 'copy',
+    },
+
+    'help' => {
+	'backup_mode' => 'move',
+    },
+
+    'help-admin' => {
+	'backup_mode' => 'move',
+    },
+
+    'objective' => {
+	'backup_mode' => 'move',
+    },
+
+    'welcome' => {
+	'backup_mode' => 'move',
+    },
+
+
+    # 
+    # include*
+    # 
+    'include' => {
+	'backup_mode' => 'copy',
+	'type'        => 'include',
+    },
+
+    'include-ctl' => {
+	'backup_mode' => 'copy',
+	'type'        => 'include',
+    },
+
+    'include-mead' => {
+	'backup_mode' => 'copy',
+	'type'        => 'include',
+    },
+
+
+    # 
+    # list files
+    # 
+
+    # member list files
+    'actives' => {
+	'backup_mode' => 'move',
+	'type'        => 'list',
+    },
+
+    'members' => {
+	'backup_mode' => 'copy',
+	'type'        => 'list',
+    },
+
+    # admin member list files
+    'members-admin' => {
+	'backup_mode' => 'copy',
+	'type'        => 'list',
+    },
+
+    # moderators
+    'moderators' => {
+	'backup_mode' => 'copy',
+	'type'        => 'list',
+    },
+
+
+    # 
+    # password files
+    # 
+    'etc/passwd' => {
+	'backup_mode' => 'move',
+	'type'        => 'list',
+    },
+
+
+    # 
+    # misc
+    # 
+    'Makefile' => {
+	'backup_mode' => 'move',
+    },
+};
+
+
+# Descriptions: return files to back up.
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: ARRAY_REF
+sub get_old_config_files
+{
+    my ($self)  = @_;
+
+    my (@files) = keys %$ml_home_dir_backup_target_files;
+    return \@files
+}
+
+
+# Descriptions: return list of include* files.
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: ARRAY_REF
+sub get_old_include_files
+{
+    my ($self)  = @_;
+    my (@files) = ();
+
+    for my $f (keys %$ml_home_dir_backup_target_files) {
+	my $type = $ml_home_dir_backup_target_files->{ $f }->{ type } || '';
+	if ($type eq 'include') {
+	    push(@files, $f);
+	}
+    }
+
+    return \@files
+}
+
+
+# Descriptions: return list of list* files.
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: ARRAY_REF
+sub get_old_list_files
+{
+    my ($self)  = @_;
+    my (@files) = ();
+
+    for my $f (keys %$ml_home_dir_backup_target_files) {
+	my $type = $ml_home_dir_backup_target_files->{ $f }->{ type } || '';
+	if ($type eq 'list') {
+	    push(@files, $f);
+	}
+    }
+
+    return \@files
+}
+
+
+sub backup_mode
+{
+    my ($self, $file) = @_;
+
+    if (defined $ml_home_dir_backup_target_files->{$file}->{backup_mode}) {
+	my $mode = $ml_home_dir_backup_target_files->{$file}->{backup_mode};
+	return $mode;
+    }
+    else {
+	return 'unknown';
+    }
+}
+
+
+=head1 CODING STYLE
+
+See C<http://www.fml.org/software/FNF/> on fml coding style guide.
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2004 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 HISTORY
+
+FML::Merge::FML4::Config appeared in fml8 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
