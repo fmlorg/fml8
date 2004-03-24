e97ade5201afe3cf7352581f7af38de25aca7d58
fukachan <fukachan>
2004-03-24 00:14:09 +0000

    fix copyright

--- fml/lib/FML/Command/Admin/spool.pm
+++ fml/lib/FML/Command/Admin/spool.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2003 Ken'ichi Fukamachi
+#  Copyright (C) 2003,2004 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: spool.pm,v 1.4 2003/08/23 04:35:32 fukachan Exp $
+# $FML: spool.pm,v 1.5 2004/03/23 04:20:46 fukachan Exp $
 #
 
 package FML::Command::Admin::spool;
@@ -107,7 +107,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2003 Ken'ichi Fukamachi
+Copyright (C) 2003,2004 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
