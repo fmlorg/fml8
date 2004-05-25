9efb402c863f27152518ef35097a668b9f24fba3
fukachan <fukachan>
2004-05-25 03:39:50 +0000

    preliminary implemenation of external filter plugin

---
+++ fml/lib/FML/Filter/External.pm
@@ -0,0 +1,96 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2004 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself.
+#
+# $FML: @template.pm,v 1.8 2004/01/01 07:29:27 fukachan Exp $
+#
+
+package FML::Filter::External;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+use ErrorStatus qw(error_set error error_clear);
+
+=head1 NAME
+
+FML::Filter::External - spawn external checker program. 
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 C<new()>
+
+=cut
+
+
+sub new
+{
+    my ($self, $curproc) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = { _curproc => $curproc };
+    return bless $me, $type;
+}
+
+
+# Descriptions: spawn external driver e.g. virus checker, spam checker.
+#    Arguments: OBJ($self) OBJ($curproc) OBJ($msg) STR($driver)
+# Side Effects: none
+# Return Value: none
+sub spawn
+{
+    my ($self, $curproc, $msg, $driver) = @_;
+    my $timeout = 120;
+    my $id      = sprintf("%s-%s", time, $$);
+
+    my $_driver = sprintf("%s::%s", "FML::Filter::External", $driver);
+    eval qq{
+        local(\$SIG{ALRM}) = sub { croak(\"\$id timeout\");};
+        alarm( \$timeout );
+
+	use $_driver;
+	my \$ext_filter = new $_driver;
+	\$ext_filter->process(\$curproc, \$msg);
+    };
+
+    my $r;
+    if ($r = $@) {
+	$curproc->logerror($@);
+	if ($r =~ /$id timeout/) {
+	    $curproc->logwarn("$driver timeout.");
+	}
+	else {
+	    $self->error_set($@);
+	}
+    }
+}
+
+
+=head1 CODING STYLE
+
+See C<http://www.fml.org/software/FNF/> on fml coding style guide.
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2004 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 HISTORY
+
+FML::Filter::External appeared in fml8 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
---
+++ fml/lib/FML/Filter/External/bogofilter.pm
@@ -0,0 +1,120 @@
+#-*- perl -*-
+#
+#  Copyright (C) 2004 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under the same terms as Perl itself.
+#
+# $FML: @template.pm,v 1.8 2004/01/01 07:29:27 fukachan Exp $
+#
+
+package FML::Filter::External::bogofilter;
+use strict;
+use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
+use Carp;
+
+=head1 NAME
+
+FML::Filter::External::bogofilter - Bogo Filter interface.
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 C<new()>
+
+=cut
+
+
+# Descriptions: constructor.
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: OBJ
+sub new
+{
+    my ($self) = @_;
+    my ($type) = ref($self) || $self;
+    my $me     = {};
+    return bless $me, $type;
+}
+
+
+# Descriptions: check if the current message looks a spam.
+#    Arguments: OBJ($self) OBJ($curproc) OBJ($msg)
+# Side Effects: none
+# Return Value: NUM(1 or 0) (1 if spam)
+sub process
+{
+    my ($self, $curproc, $msg) = @_;
+    my $config  = $curproc->config();
+    my $program = $config->{ path_bogofilter } || '';
+    my $opts    = $config->{ article_spam_filter_bogofilter_options } || '';
+    if ($program) {
+	if (-x $program) {
+	    my $_program = sprintf("%s %s", $program, $opts);
+	    return $self->_check($curproc, $msg, $_program);
+	}
+	else {
+	    $curproc->logerror("$program not exists");
+	}
+    } 
+    else {
+	$curproc->logerror("path_bogofilter undefined");
+    }
+
+    return 0;
+}
+
+
+# Descriptions: check the message through $program.
+#    Arguments: OBJ($self) OBJ($curproc) OBJ($msg) STR($program)
+# Side Effects: none
+# Return Value: NUM(1 or 0) (1 if spam)
+sub _check
+{
+    my ($self, $curproc, $msg, $program) = @_;
+
+    use FileHandle;
+    my $wh = new FileHandle "| $program";
+    if (defined $wh) {
+	$wh->autoflush(1);
+	$msg->print($wh);
+	$wh->close();
+	if ($?) {
+	    $curproc->logerror("determined as SPAM (exit code = $?)");
+	    return 1;
+	}
+	else {
+	    $curproc->log("not SPAM");
+	}
+    }
+
+    return 0;
+}
+
+
+=head1 CODING STYLE
+
+See C<http://www.fml.org/software/FNF/> on fml coding style guide.
+
+=head1 AUTHOR
+
+Ken'ichi Fukamachi
+
+=head1 COPYRIGHT
+
+Copyright (C) 2004 Ken'ichi Fukamachi
+
+All rights reserved. This program is free software; you can
+redistribute it and/or modify it under the same terms as Perl itself.
+
+=head1 HISTORY
+
+FML::Filter::External::bogofilter appeared in fml8 mailing list driver package.
+See C<http://www.fml.org/> for more details.
+
+=cut
+
+
+1;
