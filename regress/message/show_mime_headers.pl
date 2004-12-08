df6a9c31b554c74bae07f6a12f558fbe0c155cd3
fukachan <fukachan>
2004-12-08 10:49:40 +0000

    test of message_chain_as_array_ref()

--- regress/message/show_mime_headers.pl
+++ regress/message/show_mime_headers.pl
@@ -5,7 +5,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself. 
 #
-# $FML: basic_io.pl,v 1.5 2002/04/18 14:18:09 fukachan Exp $
+# $FML: show_mime_headers.pl,v 1.1 2004/12/08 07:27:33 fukachan Exp $
 #
 
 use strict;
@@ -18,13 +18,29 @@ my $test_mode = $ENV{'test_mode'} ? 1 : 0;
 my $tmp = "/tmp/buf$$";
 
 for my $f (@ARGV) {
-    my $fh  = new FileHandle $f;
-    my $wh  = new FileHandle "> $tmp";
-    my $obj = Mail::Message->parse( { fd => $fh } );
+    my $fh   = new FileHandle $f;
+    my $wh   = new FileHandle "> $tmp";
+    my $obj  = Mail::Message->parse( { fd => $fh } );
+    my $head = $obj->whole_message_header();
+    my $i    = 0;
 
-    for (my $m = $obj ; $m->{ next }; $m = $m->{ next }) { 
+    print "1. \n";
+    $i = 0;
+    for (my $m = $obj->{ next }; $m->{ next }; $m = $m->{ next }) { 
+	++$i;
+	print "($i) {";
 	print $m->{ header };
-	print "\n";
+	print "}\n";
+    }
+
+    print "2. \n";
+    $i = 0;
+    my $list = $obj->message_chain_as_array_ref();
+    for my $m (@$list) {
+	++$i;
+	print "($i) {";
+	print $m->{ header };
+	print "}\n";
     }
 
 }
