3697f25c0c525085cf85b7d5414b0a4a0c43c48b
fukachan <fukachan>
2004-12-19 11:10:51 +0000

    fix copyright

--- fml/lib/FML/Command/Admin/config.pm
+++ fml/lib/FML/Command/Admin/config.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2001,2002,2003,2004 Ken'ichi Fukamachi
+#  Copyright (C) 2004 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: config.pm,v 1.19 2004/06/26 11:39:16 fukachan Exp $
+# $FML: config.pm,v 1.1 2004/11/21 05:36:11 fukachan Exp $
 #
 
 package FML::Command::Admin::config;
@@ -87,7 +87,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2001,2002,2003,2004 Ken'ichi Fukamachi
+Copyright (C) 2004 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
