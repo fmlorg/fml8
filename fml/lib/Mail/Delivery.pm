f468f8c4d4101a0c7422aa2439cf232d1966c683
fukachan <fukachan>
2005-05-26 13:03:13 +0000

    fix typo: EMTP -> ESMTP.

--- fml/lib/Mail/Delivery.pm
+++ fml/lib/Mail/Delivery.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2000,2001,2002,2003,2004 Ken'ichi Fukamachi
+#  Copyright (C) 2000,2001,2002,2003,2004,2005 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Delivery.pm,v 1.11 2004/01/24 09:00:54 fukachan Exp $
+# $FML: Delivery.pm,v 1.12 2004/06/29 10:05:28 fukachan Exp $
 #
 
 package Mail::Delivery;
@@ -107,7 +107,7 @@ sub new
 	$pkg = 'Mail::Delivery::SMTP';
     }
     elsif ($protocol eq 'ESMTP') {
-	$pkg = 'Mail::Delivery::EMTP';
+	$pkg = 'Mail::Delivery::ESMTP';
     }
     elsif ($protocol eq 'LMTP') {
 	$pkg = 'Mail::Delivery::LMTP';
@@ -139,7 +139,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2000,2001,2002,2003,2004 Ken'ichi Fukamachi
+Copyright (C) 2000,2001,2002,2003,2004,2005 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
