e19ad4df732efb6d4cd3e857120c7ecff4674abb
fukachan <fukachan>
2005-06-05 09:55:37 +0000

    no fml- prefix

--- fml/utils/bin/show_version.sh
+++ fml/utils/bin/show_version.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: show_version.sh,v 1.1 2005/06/04 09:47:41 fukachan Exp $
 #
 
 if [ -f CHANGES.txt ];then
 	version=`egrep '^  *FML_CURRENT_VERSION' CHANGES.txt|sed 's/.*=//' `
-	echo fml-$version
+	echo $version
 else
 	exit 1
 fi
