8295ed65d40f99906e12bfb945cf51be18d09ce8
fukachan <fukachan>
2005-07-20 00:01:58 +0000

    current -> 7.98.1

--- regress/simulation/reset_lib.sh
+++ regress/simulation/reset_lib.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: reset_lib.sh,v 1.1 2003/02/11 13:23:14 fukachan Exp $
 #
 
 today=`date +%C%y%m%d`
 
-if [ ! -d /usr/local/lib/fml/current-$today/ ]; then
+version=7.98.1
+
+if [ ! -d /usr/local/lib/fml/$version-$today/ ]; then
 	sh install.sh
 fi
 
-sudo rsync -C -av lib/ /usr/local/lib/fml/current-$today/ ; 
+sudo rsync -C -av lib/ /usr/local/lib/fml/$version-$today/ ; 
 
 exit 0
