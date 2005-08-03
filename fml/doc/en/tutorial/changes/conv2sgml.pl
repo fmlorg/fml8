7bf24dc52e3d469739e0da8c868578ac5e4f64dc
fukachan <fukachan>
2005-08-03 13:22:38 +0000

    sync.

---
+++ fml/doc/en/tutorial/changes/conv2sgml.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl
+#
+# $FML: conv2sgml.pl,v 1.2 2005/06/25 12:06:58 fukachan Exp $
+#
+
+use strict;
+use Carp;
+
+my $i     = 0;
+my $key   = '';
+my $value = '';
+my $table = {};
+
+while (<>) {
+    if (/^\s*$/o || /^\#/) {
+	undef $key;
+	next;
+    }
+
+    if (/^([Q48]):\s*(.*)/) {
+	($key, $value) = ($1, $2);
+	$i++ if $key eq 'Q';
+	$table->{ $i }->{ $key } = $value;
+	next;
+    }
+
+    if (/^\s+(.*)/) {
+	$table->{ $i }->{ $key } .= "\n";
+	$table->{ $i }->{ $key } .= $1;
+    }
+}
+
+print "\n<!-- 注意!!! 自動生成されたファイル、手動編集禁止 !!! -->\n\n";
+
+for my $i (sort {$a <=> $b} keys %$table) {
+    print "<!-- $i -->\n";
+    print "<row>\n";
+
+    print "\n<entry>\n";
+    print $table->{ $i }->{ Q };
+    print "\n</entry>\n";
+
+    print "\n<entry>\n";
+    print $table->{ $i }->{ 4 };
+    print "\n</entry>\n";
+
+    print "\n<entry>\n";
+    print $table->{ $i }->{ 8 };
+    print "\n</entry>\n";
+
+    print "\n</row>\n";
+    print "\n";
+}
+
+exit 0;
