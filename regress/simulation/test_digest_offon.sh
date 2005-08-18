73e26166610a72e448c8816688eaf5bb63e33e79
fukachan <fukachan>
2005-08-18 10:35:33 +0000

    change test mail addr

--- regress/simulation/test_digest_offon.sh
+++ regress/simulation/test_digest_offon.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: test_digest_offon.sh,v 1.1 2002/12/25 15:35:34 fukachan Exp $
 #
 
 sh reset_lib.sh ; 
 
 printf "\n\n\n";
 
-makefml digest elena fukachan@sapporo.iij.ad.jp on
+makefml digest elena fukachan@home.fml.org on
 sleep 3;
 
 head /var/spool/ml/elena/recipients*
 
 printf "\n\n\n";
 
-makefml digest elena fukachan@sapporo.iij.ad.jp off
+makefml digest elena fukachan@home.fml.org off
 sleep 3;
 
 head /var/spool/ml/elena/recipients*
