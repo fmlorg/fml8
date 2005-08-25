43574d609bacfb914fe38b51b95e753806deaf22
fukachan <fukachan>
2005-08-25 13:39:45 +0000

    fix copyright

--- fml/lib/Mail/Message/Thread.pm
+++ fml/lib/Mail/Message/Thread.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2003,2004 Ken'ichi Fukamachi
+#  Copyright (C) 2003,2004,2005 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Thread.pm,v 1.6 2004/10/31 07:38:47 fukachan Exp $
+# $FML: Thread.pm,v 1.7 2005/08/25 13:13:35 fukachan Exp $
 #
 
 package Mail::Message::Thread;
@@ -260,7 +260,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2003,2004 Ken'ichi Fukamachi
+Copyright (C) 2003,2004,2005 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
