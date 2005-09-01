f56be621ef761bc9a7d266140704055bdcd1d41f
fukachan <fukachan>
2005-09-01 04:04:19 +0000

    fix copyright

--- fml/lib/Mail/Message.pm
+++ fml/lib/Mail/Message.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2001,2002,2003,2004 Ken'ichi Fukamachi
+#  Copyright (C) 2001,2002,2003,2004,2005 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Message.pm,v 1.99 2005/08/31 11:13:03 fukachan Exp $
+# $FML: Message.pm,v 1.100 2005/08/31 12:12:35 fukachan Exp $
 #
 
 package Mail::Message;
@@ -3039,7 +3039,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2001,2002,2003,2004 Ken'ichi Fukamachi
+Copyright (C) 2001,2002,2003,2004,2005 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
