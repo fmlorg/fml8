c9197cff8a13b50cb3618ca759173396a8e92d25
fukachan <fukachan>
2005-09-16 10:35:11 +0000

    reset digest sequence as article seq + 1

--- regress/simulation/emul_digest.sh
+++ regress/simulation/emul_digest.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: emul_digest.sh,v 1.1 2002/11/18 14:38:38 fukachan Exp $
 #
 
 find /var/spool/ml/elena/var/mail/queue -type f | perl -nple unlink 
 
 sh reset_lib.sh 
-echo 340 > /var/spool/ml/elena/seq-digest 
+
+j=`cat /var/spool/ml/elena/seq`
+i=`expr $j - 10`
+echo $i > /var/spool/ml/elena/seq-digest 
 
 /usr/local/libexec/fml/digest elena@home.fml.org 
 
