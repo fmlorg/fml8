4b35bb5e05f3b778bceaa8317bb5d37ef0c4306b
fukachan <fukachan>
2005-09-16 10:36:01 +0000

    generete both ja and en

--- regress/simulation/dump_curproc.sh
+++ regress/simulation/dump_curproc.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 #
-# $FML$
+# $FML: dump_curproc.sh,v 1.1 2003/03/14 04:11:52 fukachan Exp $
 #
 
-hdr=../../fml/doc/ja/tutorial/internals/CURPROC.sgml
+hdr=/tmp/buf$$
+hdr1=../../fml/doc/ja/tutorial/internals/CURPROC.sgml
+hdr2=../../fml/doc/en/tutorial/internals/CURPROC.sgml
+
+trap "rm -f $hdr" 0 1 3 15
 
 _script=emul_post.sh
 
@@ -16,6 +20,9 @@ egrep -v '^CURPROC_' |\
 tee -a $hdr
 
 printf "</screen>\n</para>\n" >> $hdr
+
+cp $hdr $hdr1
+cp $hdr $hdr2
 
 echo "";
 echo "see $hdr";
--- regress/simulation/dump_pcb.sh
+++ regress/simulation/dump_pcb.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 #
-# $FML: dump_curproc.sh,v 1.1 2003/03/14 04:11:52 fukachan Exp $
+# $FML: dump_pcb.sh,v 1.1 2003/10/29 14:50:54 fukachan Exp $
 #
 
-hdr=../../fml/doc/ja/tutorial/internals/PCB.sgml
+hdr_ja=../../fml/doc/ja/tutorial/internals/PCB.sgml
+hdr_en=../../fml/doc/en/tutorial/internals/PCB.sgml
+hdr=/tmp/pcb.$$
+
+trap "rm -f $hdr" 0 1 3 15
 
 printf "<para>\n<screen>\n" > $hdr
 
 ../style/check_pcb.pl `find FML/ -type f|grep 'pm$'` >> $hdr
 
 printf "</screen>\n</para>\n" >> $hdr
+
+cp -p $hdr $hdr_ja
+cp -p $hdr $hdr_en
 
 echo "";
 echo "see $hdr";
