b57814c5b8fd141ba338a6825b6428230dd55bed
fukachan <fukachan>
2006-02-18 08:37:43 +0000

    define add() and set()

--- fml/lib/FML/User/DB.pm
+++ fml/lib/FML/User/DB.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: DB.pm,v 1.7 2004/07/11 15:43:40 fukachan Exp $
+# $FML: DB.pm,v 1.8 2004/07/23 15:59:12 fukachan Exp $
 #
 
 package FML::User::DB;
@@ -57,13 +57,25 @@ sub new
     my ($self, $curproc, $dbargs) = @_;
     my ($type) = ref($self) || $self;
     my $me     = { _curproc => $curproc };
+
+    # initialize.
+    my $config = $curproc->config();
+    my $db_dir = $config->{ user_db_dir };
+    $curproc->mkdir($db_dir, "mode=private");
+
     return bless $me, $type;
 }
 
 
-=head2 add($class, $key, $value)
+=head2 set($class, $key, $value)
 
 add { $key => $value } info to $primary_user_db_${class}_map.
+
+=head2 get($class, $key)
+
+=head2 add($class, $key, $value)
+
+same as set() above.
 
 =cut
 
@@ -72,7 +84,7 @@ add { $key => $value } info to $primary_user_db_${class}_map.
 #    Arguments: OBJ($self) STR($class) STR($key) STR($value)
 # Side Effects: update database
 # Return Value: none
-sub add
+sub set
 {
     my ($self, $class, $key, $value) = @_;
     my $curproc = $self->{ _curproc };
@@ -96,6 +108,47 @@ sub add
     else {
 	$curproc->logerror("\$map undeflined");
     }
+}
+
+
+# Descriptions: find the first matched entry { $key => $value } 
+#               in $primary_user_db_${class}_map.
+#    Arguments: OBJ($self) STR($class) STR($key)
+# Side Effects: update database
+# Return Value: none
+sub get
+{
+    my ($self, $class, $key) = @_;
+    my $curproc = $self->{ _curproc };
+    my $config  = $debug ? $curproc->{ config } : $curproc->config();
+    my $mapname = sprintf("primary_user_db_%s_map", $class);
+    my $map     = $config->{ $mapname };
+
+    if ($map) {
+	if ($debug) { print STDERR "open map=$map\n";}
+
+	use IO::Adapter;
+	my $obj = new IO::Adapter $map;
+	$obj->open();
+	my $result = $self->find($class, $key);
+	$obj->close();
+
+	return $result;
+    }
+    else {
+	$curproc->logerror("\$map undeflined");
+    }
+}
+
+
+# Descriptions: add { $key => $value } to $primary_user_db_${class}_map.
+#    Arguments: OBJ($self) STR($class) STR($key) STR($value)
+# Side Effects: update database
+# Return Value: none
+sub add
+{
+    my ($self, $class, $key, $value) = @_;
+    $self->set($class, $key, $value);
 }
 
 
@@ -167,7 +220,8 @@ if ($0 eq __FILE__) {
 
 	my $cache_dir = '/tmp';
 	my $class     = 'gecos',
-	my $key       = 'rudo@nuinui.net';
+	my $key       = 'rudo@example.com';
+	my $key2      = 'fukachan@example.com';
 	my $value     = time;
 	my $curproc   = new FML::Process::Debug;
 	my $config    = new FML::Config;
@@ -179,7 +233,12 @@ if ($0 eq __FILE__) {
 	my $data = new FML::User::DB $curproc;
 
 	print STDERR "\n? add { $key => $value }\n";
-	$data->add($class, $key, $value);
+	$data->add($class, $key,  $value);
+	$data->add($class, $key2, time + $$);
+
+	print STDERR "\n? get( $key )\n";
+	my ($r) = $data->get($class, $key);
+	print STDERR "$r\n";
 
 	print STDERR "\n? find( $key )\n";
 	my ($r) = $data->find($class, $key);
