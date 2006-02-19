fc5eeb87cc28fe9f2792a900b8f5ff21fd66e721
fukachan <fukachan>
2006-02-19 10:29:21 +0000

    define XXX_ml_home_dir for convenience

--- regress/fml4/config.sh
+++ regress/fml4/config.sh
@@ -1,5 +1,5 @@
 #
-# $FML$
+# $FML: config.sh,v 1.1 2006/01/19 14:23:21 fukachan Exp $
 #
 
 exec_prefix=/usr/local/fml4
@@ -7,3 +7,6 @@ exec_prefix=/usr/local/fml4
 ml_home_prefix=/var/spool/ml4
 ml_home_dir=$ml_home_prefix/elena
 
+elena_ml_home_dir=$ml_home_prefix/elena
+test_ml_home_dir=$ml_home_prefix/test
+all_ml_home_dir=$ml_home_prefix/all
