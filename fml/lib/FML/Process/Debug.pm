26d286598b9ba2af7f09fa31a5d7e6efb0fa03ea
fukachan <fukachan>
2006-05-04 05:01:56 +0000

    define incoming_message(), which returns undef as a dummy.

--- fml/lib/FML/Process/Debug.pm
+++ fml/lib/FML/Process/Debug.pm
@@ -4,7 +4,7 @@
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Debug.pm,v 1.11 2006/04/17 13:36:23 fukachan Exp $
+# $FML: Debug.pm,v 1.12 2006/04/18 11:14:06 fukachan Exp $
 #
 
 package FML::Process::Debug;
@@ -198,6 +198,25 @@ sub config
     my ($self) = @_;
 
     return( defined $self->{ config } ? $self->{ config } : undef );
+}
+
+
+=head2 incoming_message()
+
+return undef since this method is a dummy.
+
+=cut
+
+
+# Descriptions: dummy. return undef.
+#    Arguments: OBJ($self)
+# Side Effects: none
+# Return Value: undef
+sub incoming_message
+{
+    my ($self) = @_;
+
+    return undef;
 }
 
 
