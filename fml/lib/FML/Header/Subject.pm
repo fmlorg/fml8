b5688e74cbf02e442002774ba1e58915dd19ec34
fukachan <fukachan>
2006-11-24 01:12:07 +0000

    fix copyright

--- fml/lib/FML/Header/Subject.pm
+++ fml/lib/FML/Header/Subject.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2001,2002,2003,2004,2005 Ken'ichi Fukamachi
+#  Copyright (C) 2001,2002,2003,2004,2005,2006 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: Subject.pm,v 1.53 2005/08/20 01:25:15 fukachan Exp $
+# $FML: Subject.pm,v 1.54 2006/11/19 05:09:13 fukachan Exp $
 #
 
 package FML::Header::Subject;
@@ -284,7 +284,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2001,2002,2003,2004,2005 Ken'ichi Fukamachi
+Copyright (C) 2001,2002,2003,2004,2005,2006 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
