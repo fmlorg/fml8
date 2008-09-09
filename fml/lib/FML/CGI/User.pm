e1c50631e3b37914194a6fba33eeda392e7cc1e1
fukachan <fukachan>
2008-09-09 08:27:40 +0000

    define anonymous_cgi_menu(). fix and update documents.

--- fml/lib/FML/CGI/User.pm
+++ fml/lib/FML/CGI/User.pm
@@ -1,10 +1,10 @@
 #-*- perl -*-
 #
-#  Copyright (C) 2002,2003,2004,2005,2006 Ken'ichi Fukamachi
+#  Copyright (C) 2002,2003,2004,2005,2006,2008 Ken'ichi Fukamachi
 #   All rights reserved. This program is free software; you can
 #   redistribute it and/or modify it under the same terms as Perl itself.
 #
-# $FML: User.pm,v 1.15 2006/03/05 08:08:36 fukachan Exp $
+# $FML: User.pm,v 1.16 2006/03/05 09:50:42 fukachan Exp $
 #
 
 package FML::CGI::User;
@@ -12,6 +12,36 @@ use strict;
 use Carp;
 use vars qw(@ISA @EXPORT @EXPORT_OK $AUTOLOAD);
 use CGI qw/:standard/; # load standard CGI routines
+
+
+=head1 NAME
+
+FML::CGI::User - manipulate user related operations for cgi scripts.
+
+=head1 SYNOPSIS
+
+    use FML::CGI::User;
+    my $submit = new FML::CGI::User;
+    $submit->cgi_menu($curproc, $command_context);
+
+    use FML::CGI::User;
+    my $submit = new FML::CGI::User;
+    $submit->anonymous_cgi_menu($curproc, $command_context);
+
+=head1 DESCRIPTION
+
+The main purpose of CGI interface provides easy manipulation of users,
+e.g. subscribe and unsubscribe.
+
+FML::CGI::User class provides the entrance for such operations.
+
+=head1 METHODS
+
+=head2 new()
+
+constructor.
+
+=cut
 
 
 # Descriptions: constructor.
@@ -25,6 +55,17 @@ sub new
     my $me     = {};
     return bless $me, $type;
 }
+
+
+=head1 METHODS for ADMINISTRATORS' CGI
+
+=head2 cgi_menu($curproc, $command_context)
+
+create a menu for the specified user related operation.
+
+This method provides a menu for administrators.
+
+=cut
 
 
 # Descriptions: show menu for user control commands such as
@@ -160,6 +201,131 @@ sub _get_address_list
 }
 
 
+=head1 ANONYMOUS CGI
+
+=head2 anonymous_cgi_menu($curproc, $command_context)
+
+create a menu for the specified user related operation.
+
+This method provides a menu for anonymous users.
+
+=cut
+
+
+# Descriptions: show the menu for anonymous user request.
+#    Arguments: OBJ($self) OBJ($curproc) HASH_REF($command_context)
+# Side Effects: none
+# Return Value: none
+sub anonymous_cgi_menu
+{
+    my ($self, $curproc, $command_context) = @_;
+    my $target  = $curproc->cgi_var_frame_target();
+    my $action  = $curproc->cgi_var_action();
+    my $comname = $command_context->{ comname };
+
+    use FML::CGI::Anonymous::DB;
+    my $db = new FML::CGI::Anonymous::DB $curproc;
+    my $session_id = $db->get_session_id();
+
+    # natural language-ed name
+    my $_mgs         = "magic string";
+    my $name_ml_name = $curproc->message_nl('term.ml_name', 'ml_name');
+    my $name_address = $curproc->message_nl('term.address', 'address');
+    my $name_magic   = $curproc->message_nl('term.magic_string', $_mgs);
+    my $name_command = $curproc->message_nl('term.command', 'command');
+    my $name_submit  = $curproc->message_nl('term.submit',  'submit');
+    my $name_reset   = $curproc->message_nl('term.reset',   'reset');
+
+    my $name_usage   = $curproc->message_nl("cgi.anonymous.$comname");
+    print "<p>$name_usage\n";
+
+    if ($comname eq 'subscribe' || $comname eq 'unsubscribe') {
+	print start_form(-action=>$action, -target=>$target);
+
+	print hidden(-name => 'session_id',   -default => [ $session_id ]);
+	print hidden(-name => 'command',      -default => [ $comname ]);
+	print hidden(-name => 'navi_command', -default => [ $comname ]);
+
+	print "<!-- new line -->\n";
+	print "<table border=0 cellspacing=\"0\" cellpadding=\"5\">\n";
+
+	print "<!-- new line -->\n";
+	print "<tr>\n";
+	print "<td>\n";
+	print $name_address;
+	print "<td>\n";
+	print textfield(-name  => 'address_specified',
+			-size  => 30);
+
+	print "<!-- new line -->\n";
+	print "<tr>\n";
+	print "<td>\n";
+	print $name_magic;
+	print "<td>\n";
+	print textfield(-name  => 'magic_string',
+			-size  => 30);
+
+	print "<!-- new line -->\n";
+	print "</table>\n";
+
+	print "<br>\n";
+	print submit(-name => $name_submit);
+	print reset(-name  => $name_reset);
+	print end_form();
+    }
+    elsif ($comname eq 'chaddr') {
+	my $name_old_address = $curproc->message_nl('term.address_old', 
+						    'old address');
+	my $name_new_address = $curproc->message_nl('term.address_new', 
+						    'new address');
+
+	print start_form(-action=>$action, -target=>$target);
+
+	print hidden(-name => 'session_id',   -default => [ $session_id ]);
+	print hidden(-name => 'command',      -default => [ $comname ]);
+	print hidden(-name => 'navi_command', -default => [ $comname ]);
+
+	print "<!-- new line -->\n";
+	print "<table border=0 cellspacing=\"0\" cellpadding=\"5\">\n";
+
+	print "<!-- new line -->\n";
+	print "<tr>\n";
+	print "<td>\n";
+	print $name_old_address;
+	print "<td>\n";
+	print textfield(-name  => 'old_address',
+			-size  => 30);
+
+	print "<!-- new line -->\n";
+	print "<tr>\n";
+	print "<td>\n";
+	print $name_new_address;
+	print "<td>\n";
+	print textfield(-name  => 'new_address',
+			-size  => 30);
+
+	print "<!-- new line -->\n";
+	print "<tr>\n";
+	print "<td>\n";
+	print $name_magic;
+	print "<td>\n";
+	print textfield(-name  => 'magic_string',
+			-size  => 30);
+
+	print "<!-- new line -->\n";
+	print "</table>\n";
+
+	print "<br>\n";
+	print submit(-name => $name_submit);
+	print reset(-name  => $name_reset);
+	print end_form();
+    }
+    else {
+	print "NOT SUPPORTED\n<br>\n";
+    }
+}
+
+
 =head1 CODING STYLE
 
 See C<http://www.fml.org/software/FNF/> on fml coding style guide.
@@ -170,7 +336,7 @@ Ken'ichi Fukamachi
 
 =head1 COPYRIGHT
 
-Copyright (C) 2002,2003,2004,2005,2006 Ken'ichi Fukamachi
+Copyright (C) 2002,2003,2004,2005,2006,2008 Ken'ichi Fukamachi
 
 All rights reserved. This program is free software; you can
 redistribute it and/or modify it under the same terms as Perl itself.
